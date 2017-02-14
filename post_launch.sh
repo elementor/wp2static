ff838bbf9fc1d7d7c3e3a6e82e81343fa36a3f13
Leon <lionhive@gmail.com>
2017-02-15 06:41:55 +1300

    bind mount dev source

--- post_launch.sh
+++ post_launch.sh
@@ -6,11 +6,13 @@ cd /var/www/html
 # install default
 wp --allow-root core install --url='172.17.0.3' --title='wp plugindev' --admin_user=admin --admin_password=admin --admin_email=blah@blah.com --skip-email
 
-# install static plugin
-wp --allow-root plugin install static-html-output-plugin --activate
+# www-data to own plugin src files which have been bound
+chown -R www-data:www-data wp-content/plugins/wordpress-static-html-output
+chown -R www-data:www-data wp-content/uploads
 
-#TODO: option to use local app folder vs pulling plugin from web
+# OPTIONAL: install latest static plugin from WP plugins site vs local src
+#wp --allow-root plugin install static-html-output-plugin --activate
 
-# run continual process to keep container alive without daemonizing
-tail -f /var/log/apache2/error.log
+# OPTIONAL: run log apache errors
+# tail -f /var/log/apache2/error.log
 
