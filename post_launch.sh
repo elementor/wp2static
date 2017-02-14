c3baaad7cafb99e2f34637f4bcb9082d94037c0b
Leon <lionhive@gmail.com>
2017-02-15 07:25:19 +1300

    bind mount to app path then sync

--- post_launch.sh
+++ post_launch.sh
@@ -6,7 +6,10 @@ cd /var/www/html
 # install default
 wp --allow-root core install --url='172.17.0.3' --title='wp plugindev' --admin_user=admin --admin_password=admin --admin_email=blah@blah.com --skip-email
 
-# www-data to own plugin src files which have been bound
+# copy plugin source files to WP path (else changing ownership will change on host)
+cp -r /app wp-content/plugins/wordpress-static-html-output
+
+# www-data to own plugin src files
 chown -R www-data:www-data wp-content/plugins/wordpress-static-html-output
 chown -R www-data:www-data wp-content/uploads
 
