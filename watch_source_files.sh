66e186f231a1083bbb02ac859799a4a911146cd1
Leon <lionhive@gmail.com>
2017-02-20 12:26:49 +1300

    todo efficient rsync

--- watch_source_files.sh
+++ watch_source_files.sh
@@ -4,5 +4,6 @@ inotifywait -rm --event modify --format '%w%f' --exclude '.*\.swp.*' /app |
 while read filename; do
     echo 'a file has changed!'
     echo $filename
+    # TODO: better here would just be to perform rsync on the one file
     . /sync_sources.sh
 done
