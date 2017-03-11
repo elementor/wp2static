bcbd3ae4405e27b672185ca4128b97884c2d3bee
Leon <lionhive@gmail.com>
2017-03-11 16:54:39 +1300

    release 1.2.1

--- publish_release.sh
+++ publish_release.sh
@@ -12,10 +12,7 @@ cp -r $PROJECT_ROOT/* $SVN_ROOT/trunk/
 # remove files only used in development
 rm $SVN_ROOT/trunk/*.sh
 rm $SVN_ROOT/trunk/*.ini
-rm $SVN_ROOT/trunk/DockerfileA
-rm -r $SVN_ROOT/trunk/.git
-rm -r $SVN_ROOT/trunk/*.swp
-rm -r $SVN_ROOT/trunk/*.swo
+rm $SVN_ROOT/trunk/Dockerfile
 rm -r $SVN_ROOT/trunk/readme.md
 
 cd $SVN_ROOT
@@ -28,3 +25,6 @@ svn ci -m "adding files for release $NEW_TAG"
 
 # svn create tag
 svn cp trunk tags/$NEW_TAG
+
+# push tag up
+svn ci -m "new tag $NEW_TAG"
