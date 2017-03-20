2d76832b41f53a1ede771cc21b32a2e727ac26fa
Leon <lionhive@gmail.com>
2017-03-20 18:34:42 +1300

    WIP circle ci docker auto tests
    Former-commit-id: 5a2fb392d2d751374a53dbdb3d89b29b657d66bf

--- run_tests.rb
+++ run_tests.rb
@@ -3,42 +3,53 @@ require 'rspec/expectations'
 include RSpec::Matchers
 
 def setup
-  caps = Selenium::WebDriver::Remote::Capabilities.send("chrome")
-  @driver = Selenium::WebDriver.for(:remote, url: "http://0.0.0.0:4444/wd/hub", desired_capabilities: caps)
-  @driver.manage.window.size = Selenium::WebDriver::Dimension.new(1920, 1080)
+    caps = Selenium::WebDriver::Remote::Capabilities.send("chrome")
+    @driver = Selenium::WebDriver.for(:remote, url: "http://0.0.0.0:4444/wd/hub", desired_capabilities: caps)
+    @driver.manage.window.size = Selenium::WebDriver::Dimension.new(1920, 1080)
 end
 
 def teardown
-  @driver.quit
+    @driver.quit
 end
 
 def run
-  setup
-  yield
-  teardown
+    setup
+    yield
+    teardown
 end
 
 run do
-  container_ip = ARGV[0]
-    
-  puts container_ip
+    container_ip = ARGV[0]
 
-  # Open the main page and check for the title
-  @driver.get "http://#{container_ip}/"
-  #@driver.save_screenshot(File.join(Dir.pwd, "selenium-docker-main-page.png"))
-  #@driver.save_screenshot(File.join(Dir.pwd, "selenium-docker-main-page.png"))
-  expect(@driver.title).to eql 'wp plugindev – Just another WordPress site'
+    puts "Running tests against container IP: #{container_ip}"
 
-  puts 'title test OK'
+    # Open the main page and check for the title
+    site_url = "http://#{container_ip}"
+    @driver.get site_url + '/'
+    #@driver.save_screenshot(File.join(Dir.pwd, "selenium-docker-main-page.png"))
+    expect(@driver.title).to eql 'wp plugindev – Just another WordPress site'
 
-  
+    puts 'Title test OK'
 
 
-  ## Open the chebox page and check if the last checkbox is "checked"
-  #@driver.get 'http://the-internet.herokuapp.com/checkboxes'
-  #checkboxes = @driver.find_elements(css: 'input[type="checkbox"]')
-  #expect(checkboxes.last.selected?).to eql true
+    @driver.get site_url + '/wp-admin'
 
-  ## Generate a screenshot of the checkbox page
-  #@driver.save_screenshot(File.join(Dir.pwd, "selenium-docker-checkbox-page.png"))
+    @driver.find_element(name: 'log').send_keys 'admin'
+    @driver.find_element(name: 'pwd').send_keys 'admin'
+    @driver.find_element(name: 'wp-submit').submit
+
+    expect(@driver.title).to eql 'Dashboard ‹ wp plugindev — WordPress'
+
+    puts 'Login test OK'
+
+    @driver.get site_url + '/wp-admin/tools.php?page=wp-static-html-output-options'
+
+    # setup export and run
+
+    # get list of files from export folder (should be only one exported folder)
+
+    # check contents of index.html file
+
+    ## Generate a screenshot of the checkbox page
+    @driver.save_screenshot(File.join(Dir.pwd, "selenium-docker-plugin-settings.png"))
 end
