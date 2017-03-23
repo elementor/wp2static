2d5c6beb66bc6253d7e24482d380a26409823e35
Leon <lionhive@gmail.com>
2017-03-23 18:51:45 +1300

    debug testing in circle
    Former-commit-id: c9e8217949503eb65957bcd9634b72e9b0f5d923

--- run_tests.sh
+++ run_tests.sh
@@ -2,10 +2,18 @@
 echo 'what is running now?'
 docker ps
 
-containerID=$(sudo docker ps | grep plugindevwp | grep -o -e '^\S*')
+webContainerID=$(sudo docker ps | grep plugindevwp | grep -o -e '^\S*')
+sqlContainerID=$(sudo docker ps | grep devmysql | grep -o -e '^\S*')
+seleniumContainerID=$(sudo docker ps | grep seleniumserver  | grep -o -e '^\S*')
 
-containerIP=$( sudo docker inspect --format="{{ .NetworkSettings.IPAddress }}" $containerID)
+webContainerIP=$( sudo docker inspect --format="{{ .NetworkSettings.IPAddress }}" $webContainerID)
+sqlContainerIP=$( sudo docker inspect --format="{{ .NetworkSettings.IPAddress }}" $sqlContainerID)
+seleniumContainerIP=$( sudo docker inspect --format="{{ .NetworkSettings.IPAddress }}" $seleniumContainerID)
+
+echo $webContainerIP
+echo $sqlContainerIP
+echo $seleniumContainerIP
 
 gem install bundler
 bundle install
-bundle exec ruby run_tests.rb $containerIP
+bundle exec ruby run_tests.rb $webContainerIP
