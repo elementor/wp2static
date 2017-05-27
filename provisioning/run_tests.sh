32b7fcb6f1e0a9554a72256902c5b0c5848a9851
Leon <lionhive@gmail.com>
2017-05-27 13:56:50 +1200

    chg container ports

--- provisioning/run_tests.sh
+++ provisioning/run_tests.sh
@@ -6,7 +6,7 @@ sudo docker rm -f seleniumserver
 sudo docker build -t leonstafford/wordpress-static-html-plugin:latest .
 sudo docker run --name seleniumserver -d -p 4444:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome:latest
 sudo docker run --name devmysql -e MYSQL_ROOT_PASSWORD=banana -d mariadb
-sudo docker run --name plugindevwp --link devmysql:mysql -p 8080:80 -d -v $(pwd):/app leonstafford/wordpress-static-html-plugin
+sudo docker run --name plugindevwp --link devmysql:mysql -p 8088:80 -d -v $(pwd):/app leonstafford/wordpress-static-html-plugin
 sudo docker exec plugindevwp sh /post_launch.sh
 
 echo 'what is running now?'
