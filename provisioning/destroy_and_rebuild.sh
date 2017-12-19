c220ab18298f674095179e45e0f3591a5c3cccb0
Leon <lionhive@gmail.com>
2017-12-19 05:10:41 +0000

    update provisioning scripts
     - allow serving to an external hostname
     - quieten rsync output

--- provisioning/destroy_and_rebuild.sh
+++ provisioning/destroy_and_rebuild.sh
@@ -5,6 +5,6 @@ sudo docker rm -f devmysql
 sudo docker rm -f plugindevwp
 sudo docker build -t leonstafford/wordpress-static-html-plugin:latest . 
 sudo docker run --name devmysql -e MYSQL_ROOT_PASSWORD=banana -d mariadb
-sudo docker run --name plugindevwp --link devmysql:mysql -p 8088:80 -d -v $(pwd):/app leonstafford/wordpress-static-html-plugin
-sudo docker exec plugindevwp sh /post_launch.sh
+sudo docker run --name plugindevwp --link devmysql:mysql -p 8091:80 -d -v $(pwd):/app leonstafford/wordpress-static-html-plugin
+sudo docker exec plugindevwp bash /post_launch.sh
 sudo docker exec -it plugindevwp sh /watch_source_files.sh
