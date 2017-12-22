69dd4fb5f6771a73fdb783926b46210a8538b025
Leon <lionhive@gmail.com>
2017-12-22 12:41:35 +0000

    GitHub export
     - export to publish a static site on GitHub Pages
     - requires Personal Access Token from GitHub
    
    
    Former-commit-id: c6b59d12ca8b2158747cf80bf3abdd2b6d371b34

---
+++ library/Github/knplabs/github-api/lib/Github/Api/GitData/Trees.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Github\Api\GitData;
+
+use Github\Api\AbstractApi;
+use Github\Exception\MissingArgumentException;
+
+/**
+ * @link   http://developer.github.com/v3/git/trees/
+ * @author Joseph Bielawski <stloyd@gmail.com>
+ */
+class Trees extends AbstractApi
+{
+    /**
+     * Get the tree for a repository.
+     *
+     * @param string $username
+     * @param string $repository
+     * @param string $sha
+     * @param bool   $recursive
+     *
+     * @return array
+     */
+    public function show($username, $repository, $sha, $recursive = false)
+    {
+        return $this->get('/repos/'.rawurlencode($username).'/'.rawurlencode($repository).'/git/trees/'.rawurlencode($sha), $recursive ? array('recursive' => 1) : array());
+    }
+
+    /**
+     * Create tree for a repository.
+     *
+     * @param string $username
+     * @param string $repository
+     * @param array  $params
+     *
+     * @return array
+     *
+     * @throws \Github\Exception\MissingArgumentException
+     */
+    public function create($username, $repository, array $params)
+    {
+        if (!isset($params['tree']) || !is_array($params['tree'])) {
+            throw new MissingArgumentException('tree');
+        }
+
+        if (!isset($params['tree'][0])) {
+            $params['tree'] = array($params['tree']);
+        }
+
+        foreach ($params['tree'] as $key => $tree) {
+            if (!isset($tree['path'], $tree['mode'], $tree['type'])) {
+                throw new MissingArgumentException(array("tree.$key.path", "tree.$key.mode", "tree.$key.type"));
+            }
+
+            // If `sha` is not set, `content` is required
+            if (!isset($tree['sha']) && !isset($tree['content'])) {
+                throw new MissingArgumentException("tree.$key.content");
+            }
+        }
+
+        return $this->post('/repos/'.rawurlencode($username).'/'.rawurlencode($repository).'/git/trees', $params);
+    }
+}
---
+++ library/Github/knplabs/github-api/lib/Github/Api/Repository/Commits.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Github\Api\Repository;
+
+use Github\Api\AbstractApi;
+
+/**
+ * @link   http://developer.github.com/v3/repos/commits/
+ * @author Joseph Bielawski <stloyd@gmail.com>
+ */
+class Commits extends AbstractApi
+{
+    public function all($username, $repository, array $params)
+    {
+        return $this->get('/repos/'.rawurlencode($username).'/'.rawurlencode($repository).'/commits', $params);
+    }
+
+    public function compare($username, $repository, $base, $head, $mediaType = null)
+    {
+        $headers = array();
+        if (null !== $mediaType) {
+            $headers['Accept'] = $mediaType;
+        }
+
+        return $this->get('/repos/'.rawurlencode($username).'/'.rawurlencode($repository).'/compare/'.rawurlencode($base).'...'.rawurlencode($head), array(), $headers);
+    }
+
+    public function show($username, $repository, $sha)
+    {
+        return $this->get('/repos/'.rawurlencode($username).'/'.rawurlencode($repository).'/commits/'.rawurlencode($sha));
+    }
+}
