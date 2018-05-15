d01fefb3d372dda5657214e88f2ab49c1a84c86f
Leon <leon.stafford@mac.com>
2018-05-15 23:41:44 +0930

    prep files for 2.3 release
    Former-commit-id: 812de0a4dd05916bf1679572843ffae862395d76

--- library/StaticHtmlOutput.php
+++ library/StaticHtmlOutput.php
@@ -12,7 +12,7 @@ use Kunnu\Dropbox\Exceptions\DropboxClientException;
 use GuzzleHttp\Client;
 
 class StaticHtmlOutput {
-	const VERSION = '2.2';
+	const VERSION = '2.3';
 	const OPTIONS_KEY = 'wp-static-html-output-options';
 	const HOOK = 'wp-static-html-output';
 
--- wp-static-html-output.php
+++ wp-static-html-output.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Static HTML Output
 Plugin URI:  https://leonstafford.github.io
 Description: Benefit from WordPress as a CMS but with the speed, performance and portability of a static site
-Version:     2.2
+Version:     2.3
 Author:      Leon Stafford
 Author URI:  https://leonstafford.github.io
 Text Domain: static-html-output-plugin
