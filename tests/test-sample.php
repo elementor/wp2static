d8c6a1dbef9786b3f291afd0de2a9c118345d180
Leon <leon.stafford@mac.com>
2018-05-20 20:21:59 +0930

    WIP phpunit addition

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Wordpress_Static_Html_Output
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
