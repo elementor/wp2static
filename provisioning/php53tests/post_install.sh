3dae86abcef4fe65b2c6d00e21e31596862d7a71
Leon <leon.stafford@mac.com>
2018-05-20 22:51:17 +0930

    Php 5.3.29 docker container working

---
+++ provisioning/php53tests/post_install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+# install WP CLI
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+
+
+echo 'awaiting mysql to be reachable'
+
+while ! mysqladmin ping -h php53sql --silent; do
+    printf "."
+    sleep 1
+done
+
+# still requires buffer before accessible for wp cli
+sleep 5
+
+
+# install default
+cd /var/www/html
+
+rm index.html
+
+service apache2 start
+
+wp --allow-root core download 
+wp --allow-root config create --dbname=wordpress --dbuser=root --dbpass=banana --dbhost=php53sql
+
+wp --allow-root db create
+
+wp --allow-root core install --url="172.19.0.6" --title='PHP 5.3.29 WordPress Instance' --admin_user=admin --admin_password=admin --admin_email=blah@blah.com --skip-email
+
+
+
+# keep container alive
+
+while true
+do
+    sleep 1
+
+done
