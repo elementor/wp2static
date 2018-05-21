fc35aa0f99de350b54f1ddbb5ad344d02184a751
Leon <leon.stafford@mac.com>
2018-05-22 00:09:09 +0930

    simplify php 5.3.29 containerization
    Former-commit-id: ba7553dea4e38057aa2abf9de2b63c73a50c1109

--- provisioning/php53tests/run_sql.sh
+++ provisioning/php53tests/run_sql.sh
@@ -5,8 +5,9 @@ sudo docker rm -f plugindev53
 sudo docker network rm php53net
 sudo docker network create --subnet=172.19.0.0/16 php53net
 sudo docker run --name php53sql --net php53net -e MYSQL_ROOT_PASSWORD=banana -d mariadb  
-sudo docker build -t leonstafford/wordpress-static-html-plugin:latest . 
-sudo docker run --name plugindev53 --net php53net --link php53sql:mysql -p 8093:80 --ip 172.19.0.6 -d -v $(pwd):/app leonstafford/wordpress-static-html-plugin 
+sudo docker build -t leonstafford/wordpress-static-html-plugin-php53:latest . 
+# this container is simpler, exits quicker, so need to give it the loop to stay alive..
+sudo docker run --name plugindev53 --net php53net --link php53sql:mysql -p 8093:80 --ip 172.19.0.6 -d -v $(pwd):/app leonstafford/wordpress-static-html-plugin-php53 tail -f /dev/null
 
 sudo docker exec plugindev53 bash /post_install.sh
 
