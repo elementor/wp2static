4022703a0091d5629c3881c781c824136cc3be56
Leon <leon.stafford@mac.com>
2018-05-22 23:23:29 +0930

    reliably provision the 5.3.29 container
    Former-commit-id: 62e15b97fd4050382dfaf0659ac0276c711069c0

--- provisioning/php53tests/post_install.sh
+++ provisioning/php53tests/post_install.sh
@@ -11,50 +11,6 @@ done
 sleep 5
 
 
-# need apache2 sources for php
-apt-get install -y apache2 apache2-dev libxml2-dev build-essential libcurl4-openssl-dev 
-
-#mkdir /etc/apache2/mods-available
-
-
-
-cd 
-wget http://au1.php.net/distributions/php-5.3.29.tar.gz
-tar xfz php-5.3.29.tar.gz
-cd php-5.3.29
-./configure --with-mysql --with-apxs2=/usr/bin/apxs2 --with-openssl --with-curl --with-openssl-dir=/usr/bin --enable-zip --enable-mbstring --with-zlib
-make
-make install
-
-
-# need apache2 sources for php
-
-# stop complaining about php and apache being differently threaded
-#RUN a2dismod mpm_event
-#RUN a2enmod mpm_prefork
-#
-#RUN a2enmod \
-#            php5 \
-#        rewrite \
-#        ssl
-
-# stop complaining about php and apache being differently threaded
-#a2dismod mpm_event
-#a2enmod mpm_prefork
-
-echo 'AddType application/x-httpd-php .php' >> /etc/apache2/apache2.conf 
-
-apachectl stop
-apachectl start
-
-
-# install WP CLI
-
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-
-
 
 # install default
 cd /var/www
@@ -68,15 +24,15 @@ wp --allow-root core install --url="172.19.0.6" --title='PHP 5.3.29 WordPress In
 # activate wp static output plugin
 #wp --allow-root plugin activate wordpress-static-html-output
 
+rm /var/www/index.html
+
 chown -R www-data:www-data /var/www
 
-apachectl stop
-apachectl start
+service apache2 start
 
 # keep container alive
 
 while true
 do
     sleep 1
-
 done
