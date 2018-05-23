8e7c6894f5d91332c33b5f4872358e964ded24f1
Leon <leon.stafford@mac.com>
2018-05-23 22:56:49 +0930

    provision multi test environments same script
    Former-commit-id: f7b4460c5179f40932e1ae34c73ac67ef97e267d

--- provisioning/destroy_and_rebuild.sh
+++ provisioning/destroy_and_rebuild.sh
@@ -7,4 +7,6 @@ sudo docker build -t leonstafford/wordpress-static-html-plugin:latest .
 sudo docker run --name devmysql -e MYSQL_ROOT_PASSWORD=banana -d mariadb
 sudo docker run --name plugindevwp --link devmysql:mysql -p 8091:80 -d -v $(pwd):/app leonstafford/wordpress-static-html-plugin
 sudo docker exec plugindevwp bash /post_launch.sh
-sudo docker exec -it plugindevwp sh /watch_source_files.sh
+sudo docker exec -it plugindevwp sh /watch_source_files.sh &
+
+/bin/bash provisioning/php53tests/destroy_and_rebuild.sh
