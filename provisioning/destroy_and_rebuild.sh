4d56ba616d07349767e86e1689d24010795540bf
Leon <leon.stafford@mac.com>
2018-06-23 11:06:19 +0930

    allow subdir installations via env var

--- provisioning/destroy_and_rebuild.sh
+++ provisioning/destroy_and_rebuild.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+# get vars from .env-vars
+. ./provisioning/.env-vars
+
+INSTALL_PATH_OVERRIDE=
+
+if [ -z "${SUBDIR_TO_INSTALL}" ]; then 
+	echo "Installing into root"; 
+else 
+	echo "Installing into subdirectory: ${SUBDIR_TO_INSTALL}"; 
+	INSTALL_PATH_OVERRIDE=" -w=/var/www/html/${SUBDIR_TO_INSTALL}"
+fi
+
 # convenience script to destroy any running containers, rebuild (with cache) and output notifications from script watching/syncing source files
 sudo docker rm -f devmysql
 sudo docker rm -f plugindevwp
 sudo docker build -t leonstafford/wordpress-static-html-plugin:latest . 
 sudo docker run --name devmysql -e MYSQL_ROOT_PASSWORD=banana -d mariadb
-sudo docker run --name plugindevwp --env-file ./provisioning/.env-vars --link devmysql:mysql -p 8091:80 -d -v $(pwd):/app leonstafford/wordpress-static-html-plugin
+sudo docker run --name plugindevwp --env-file ./provisioning/.env-vars --link devmysql:mysql $INSTALL_PATH_OVERRIDE -p 8091:80 -d -v $(pwd):/app leonstafford/wordpress-static-html-plugin
 sudo docker exec plugindevwp bash /post_launch.sh
 sudo docker exec -it plugindevwp sh /watch_source_files.sh
 
