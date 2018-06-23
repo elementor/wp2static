9ee9f47cf36b2efba50cfd374364b5ddb3b438d9
Leon <leon.stafford@mac.com>
2018-06-23 22:15:12 +0930

    moving dir scanner to files helper class

--- library/StaticHtmlOutput.php
+++ library/StaticHtmlOutput.php
@@ -848,29 +848,6 @@ class StaticHtmlOutput {
 		}
     }
 
-	public function recursively_scan_dir($dir, $siteroot, $file_list_path){
-		// rm duplicate slashes in path (TODO: fix cause)
-		$dir = str_replace('//', '/', $dir);
-		$files = scandir($dir);
-
-		foreach($files as $item){
-			if($item != '.' && $item != '..' && $item != '.git'){
-				if(is_dir($dir.'/'.$item)) {
-					$this->recursively_scan_dir($dir.'/'.$item, $siteroot, $file_list_path);
-				} else if(is_file($dir.'/'.$item)) {
-					$subdir = str_replace('/wp-admin/admin-ajax.php', '', $_SERVER['REQUEST_URI']);
-					$subdir = ltrim($subdir, '/');
-					$clean_dir = str_replace($siteroot . '/', '', $dir.'/');
-					$clean_dir = str_replace($subdir, '', $clean_dir);
-					$filename = $dir .'/' . $item . "\n";
-					$filename = str_replace('//', '/', $filename);
-					$this->wsLog('FILE TO ADD:');
-					$this->wsLog($filename);
-					file_put_contents($file_list_path, $filename, FILE_APPEND | LOCK_EX);
-				} 
-			}
-		}
-	}
 
 	public function prepare_file_list($export_target) {
         $this->wsLog($export_target . ' EXPORT: Preparing list of files to export');
@@ -888,7 +865,7 @@ class StaticHtmlOutput {
         $archiveName = rtrim($archiveDir, '/');
         $siteroot = $archiveName . '/';
 
-        $this->recursively_scan_dir($siteroot, $siteroot, $file_list_path);
+        StaticHtmlOutput_FilesHelper::recursively_scan_dir($siteroot, $siteroot, $file_list_path);
         $this->wsLog('GENERIC EXPORT: File list prepared');
 	}
 
--- library/StaticHtmlOutput/FilesHelper.php
+++ library/StaticHtmlOutput/FilesHelper.php
@@ -17,10 +17,34 @@ class StaticHtmlOutput_FilesHelper
 		$files = array_diff(scandir($dir), array('.','..')); 
 
 		foreach ($files as $file) { 
-			(is_dir("$dir/$file")) ? StaticHtmlOutput_FilesHelper::delete_dir_with_files("$dir/$file") : unlink("$dir/$file"); 
+			(is_dir("$dir/$file")) ? self::delete_dir_with_files("$dir/$file") : unlink("$dir/$file"); 
 		} 
 
 		return rmdir($dir); 
 	} 
+
+	public static function recursively_scan_dir($dir, $siteroot, $file_list_path){
+		// rm duplicate slashes in path (TODO: fix cause)
+		$dir = str_replace('//', '/', $dir);
+		$files = scandir($dir);
+
+		foreach($files as $item){
+			if($item != '.' && $item != '..' && $item != '.git'){
+				if(is_dir($dir.'/'.$item)) {
+					self::recursively_scan_dir($dir.'/'.$item, $siteroot, $file_list_path);
+				} else if(is_file($dir.'/'.$item)) {
+					$subdir = str_replace('/wp-admin/admin-ajax.php', '', $_SERVER['REQUEST_URI']);
+					$subdir = ltrim($subdir, '/');
+					$clean_dir = str_replace($siteroot . '/', '', $dir.'/');
+					$clean_dir = str_replace($subdir, '', $clean_dir);
+					$filename = $dir .'/' . $item . "\n";
+					$filename = str_replace('//', '/', $filename);
+					//$this->wsLog('FILE TO ADD:');
+					//$this->wsLog($filename);
+					file_put_contents($file_list_path, $filename, FILE_APPEND | LOCK_EX);
+				} 
+			}
+		}
+	}
 
 }
