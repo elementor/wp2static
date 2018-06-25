cce8f9aa9721c414ecbd0313f3465ab1a97a36a8
Leon <leon.stafford@mac.com>
2018-06-25 22:18:35 +0930

    strip comments from Monolog dir
    Former-commit-id: 86b2da72375c8d82c319ddcb00f03651d47462f8

--- library/aws/Monolog/Formatter/WildfireFormatter.php
+++ library/aws/Monolog/Formatter/WildfireFormatter.php
@@ -1,32 +1,9 @@
 <?php
-
-/*
- * This file is part of the Monolog package.
- *
- * (c) Jordi Boggiano <j.boggiano@seld.be>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Monolog\Formatter;
-
 use Monolog\Logger;
-
-/**
- * Serializes a log message according to Wildfire's header requirements
- *
- * @author Eric Clemmons (@ericclemmons) <eric@uxdriven.com>
- * @author Christophe Coevoet <stof@notk.org>
- * @author Kirill chEbba Chebunin <iam@chebba.org>
- */
 class WildfireFormatter extends NormalizerFormatter
 {
     const TABLE = 'table';
-
-    /**
-     * Translates Monolog log levels to Wildfire levels.
-     */
     private $logLevels = array(
         Logger::DEBUG     => 'LOG',
         Logger::INFO      => 'INFO',
@@ -37,13 +14,8 @@ class WildfireFormatter extends NormalizerFormatter
         Logger::ALERT     => 'ERROR',
         Logger::EMERGENCY => 'ERROR',
     );
-
-    /**
-     * {@inheritdoc}
-     */
     public function format(array $record)
     {
-        // Retrieve the line and file if set and remove them from the formatted extra
         $file = $line = '';
         if (isset($record['extra']['file'])) {
             $file = $record['extra']['file'];
@@ -53,7 +25,6 @@ class WildfireFormatter extends NormalizerFormatter
             $line = $record['extra']['line'];
             unset($record['extra']['line']);
         }
-
         $record = $this->normalize($record);
         $message = array('message' => $record['message']);
         $handleError = false;
@@ -68,7 +39,6 @@ class WildfireFormatter extends NormalizerFormatter
         if (count($message) === 1) {
             $message = reset($message);
         }
-
         if (isset($record['context'][self::TABLE])) {
             $type  = 'TABLE';
             $label = $record['channel'] .': '. $record['message'];
@@ -77,8 +47,6 @@ class WildfireFormatter extends NormalizerFormatter
             $type  = $this->logLevels[$record['level']];
             $label = $record['channel'];
         }
-
-        // Create JSON object describing the appearance of the message in the console
         $json = $this->toJson(array(
             array(
                 'Type'  => $type,
@@ -88,26 +56,21 @@ class WildfireFormatter extends NormalizerFormatter
             ),
             $message,
         ), $handleError);
-
-        // The message itself is a serialization of the above JSON object + it's length
         return sprintf(
             '%s|%s|',
             strlen($json),
             $json
         );
     }
-
     public function formatBatch(array $records)
     {
         throw new \BadMethodCallException('Batch formatting does not make sense for the WildfireFormatter');
     }
-
     protected function normalize($data)
     {
         if (is_object($data) && !$data instanceof \DateTime) {
             return $data;
         }
-
         return parent::normalize($data);
     }
 }
--- library/aws/Monolog/Handler/ZendMonitorHandler.php
+++ library/aws/Monolog/Handler/ZendMonitorHandler.php
@@ -1,30 +1,9 @@
 <?php
-/*
- * This file is part of the Monolog package.
- *
- * (c) Jordi Boggiano <j.boggiano@seld.be>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Monolog\Handler;
-
 use Monolog\Formatter\NormalizerFormatter;
 use Monolog\Logger;
-
-/**
- * Handler sending logs to Zend Monitor
- *
- * @author  Christian Bergau <cbergau86@gmail.com>
- */
 class ZendMonitorHandler extends AbstractProcessingHandler
 {
-    /**
-     * Monolog level / ZendMonitor Custom Event priority map
-     *
-     * @var array
-     */
     protected $levelMap = array(
         Logger::DEBUG     => 1,
         Logger::INFO      => 2,
@@ -35,14 +14,6 @@ class ZendMonitorHandler extends AbstractProcessingHandler
         Logger::ALERT     => 7,
         Logger::EMERGENCY => 0,
     );
-
-    /**
-     * Construct
-     *
-     * @param  int                       $level
-     * @param  bool                      $bubble
-     * @throws MissingExtensionException
-     */
     public function __construct($level = Logger::DEBUG, $bubble = true)
     {
         if (!function_exists('zend_monitor_custom_event')) {
@@ -50,10 +21,6 @@ class ZendMonitorHandler extends AbstractProcessingHandler
         }
         parent::__construct($level, $bubble);
     }
-
-    /**
-     * {@inheritdoc}
-     */
     protected function write(array $record)
     {
         $this->writeZendMonitorCustomEvent(
@@ -62,32 +29,14 @@ class ZendMonitorHandler extends AbstractProcessingHandler
             $record['formatted']
         );
     }
-
-    /**
-     * Write a record to Zend Monitor
-     *
-     * @param int    $level
-     * @param string $message
-     * @param array  $formatted
-     */
     protected function writeZendMonitorCustomEvent($level, $message, $formatted)
     {
         zend_monitor_custom_event($level, $message, $formatted);
     }
-
-    /**
-     * {@inheritdoc}
-     */
     public function getDefaultFormatter()
     {
         return new NormalizerFormatter();
     }
-
-    /**
-     * Get the level map
-     *
-     * @return array
-     */
     public function getLevelMap()
     {
         return $this->levelMap;
