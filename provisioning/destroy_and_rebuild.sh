2e81973f8c170542423c6732528d20eaa6b966a8
Leon <leon.stafford@mac.com>
2018-07-02 20:37:36 +0930

    allow settings to be reset by user
    Former-commit-id: 174d3d6c201cacbd51f8961f288d82f500c9ef00

--- provisioning/destroy_and_rebuild.sh
+++ provisioning/destroy_and_rebuild.sh
@@ -13,11 +13,11 @@ else
 fi
 
 # convenience script to destroy any running containers, rebuild (with cache) and output notifications from script watching/syncing source files
-sudo docker rm -f devmysql
-sudo docker rm -f plugindevwp
+sudo docker rm -f {devmysql,plugindevwp,phpmyadmin}
 sudo docker build -t leonstafford/wordpress-static-html-plugin:latest . 
 sudo docker run --name devmysql -e MYSQL_ROOT_PASSWORD=banana -d mariadb
 sudo docker run --name plugindevwp --env-file ./provisioning/.env-vars --link devmysql:mysql $INSTALL_PATH_OVERRIDE -p 8091:80 -d -v $(pwd):/app leonstafford/wordpress-static-html-plugin
+sudo docker run --name phpmyadmin -d --link devmysql:db -p 3008:80 phpmyadmin/phpmyadmin
 sudo docker exec plugindevwp bash /post_launch.sh
 sudo docker exec -it plugindevwp sh /watch_source_files.sh
 
