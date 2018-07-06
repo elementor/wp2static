69185693fd37fd96ed371ff893f6388e8df2050e
Leon <leon.stafford@mac.com>
2018-07-07 06:37:18 +0930

    WIP failing test for settings retention
    Former-commit-id: 62b8d4e5f9b948e74436a1e5db8e5401a3aaec83

--- setests/test.php
+++ setests/test.php
@@ -37,12 +37,44 @@ class WPStaticHtmlOutputPluginTest extends TestCase {
 
     protected $url = 'http://172.17.0.3/wp-admin/';
 
-    public function testAdminWorkInProgress()
-    {
-
+    public function testAdminWorkInProgress() {
         $this->webDriver->get($this->url);
-		//$driver->get( 'http://172.17.0.3/wp-admin/' );
 		$this->assertContains('Log In ‹ wp plugindev — WordPress', $this->webDriver->getTitle());
+    }    
+
+	public function logInToAdmin() {
+        $this->webDriver->get($this->url);
+
+		// insert username #user_login
+
+		// insert password #user_pass
+
+		// submit and wait for dashboard to be visible submit #loginform
+	}	
+
+	public function resetPluginSettingsToDefault() {
+		$this->logInToAdmin();
+
+        $this->webDriver->get('http://172.17.0.3/wp-admin/tools.php?page=wp-static-html-output-options');
+	
+		// .resetDefaultSettingsButton
+	}	
+
+    public function testSavedDeploymentMethodIsRetained() {
+
+		$this->resetPluginSettingsToDefault();
+
+		$deployment_chooser = $driver->findElement(WebDriverBy::name('selected_deployment_option'));
+
+		$deployment_chooser_select = new WebDriverSelect($deployment_chooser);
+
+		$this->assertContains(
+			'Log In ‹ wp plugindev — WordPress',
+			$deployment_chooser_select->getFirstSelectedOption()->getAttribute('value'));
+
+		$this->assertContains(
+			'Log In ‹ wp plugindev — WordPress',
+			$deployment_chooser_select->getFirstSelectedOption()->getText());
     }    
 
 	public function tearDown() {
