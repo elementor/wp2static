742d790c756bfd9efa08c61d3554c987c423fbc4
Leon <leon.stafford@mac.com>
2018-07-07 00:23:48 +0930

    green test;issue with destroy rebuild script

--- provisioning/destroy_and_rebuild.sh
+++ provisioning/destroy_and_rebuild.sh
@@ -19,14 +19,14 @@ sudo docker rm -f {devmysql,plugindevwp,phpmyadmin,selenium-hub,sechrome,seffox}
 
 # setup a Selenium grid for E2E tests
 # https://robotninja.com/blog/introduction-using-selenium-docker-containers-end-end-testing/
-sudo docker run -d -p 4444:4444 --name selenium-hub selenium/hub:latest
-sudo docker run --name sechrome -d --link selenium-hub:hub selenium/node-chrome:latest
-sudo docker run --name seffox -d --link selenium-hub:hub selenium/node-firefox:latest
+# sudo docker run -d -p 4444:4444 --name selenium-hub selenium/hub:latest
+# sudo docker run --name sechrome -d --link selenium-hub:hub selenium/node-chrome:latest
+# sudo docker run --name seffox -d --link selenium-hub:hub selenium/node-firefox:latest
 
 sudo docker build -t leonstafford/wordpress-static-html-plugin:latest . 
 sudo docker run --name devmysql -e MYSQL_ROOT_PASSWORD=banana -d mariadb
 sudo docker run --name plugindevwp --env-file ./provisioning/.env-vars --link devmysql:mysql $INSTALL_PATH_OVERRIDE -p 8091:80 -d -v $(pwd):/app leonstafford/wordpress-static-html-plugin
-sudo docker run --name phpmyadmin -d --link devmysql:db -p 3008:80 phpmyadmin/phpmyadmin
+#sudo docker run --name phpmyadmin -d --link devmysql:db -p 3008:80 phpmyadmin/phpmyadmin
 sudo docker exec plugindevwp bash /post_launch.sh
 sudo docker exec -it plugindevwp sh /watch_source_files.sh
 
--- setests/run_se_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-php composer.phar install
-
-./vendor/bin/phpunit --bootstrap vendor/autoload.php test.php
