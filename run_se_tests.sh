7fc37374370a7e472e47aa9a8a0fe10f85b334d7
Leon <leon.stafford@mac.com>
2018-07-17 06:28:53 +0930

    wip - failing test for #130

--- run_se_tests.sh
+++ run_se_tests.sh
@@ -17,4 +17,4 @@ sleep 2
 
 php composer.phar install
 
-./vendor/bin/phpunit --testdox --bootstrap vendor/autoload.php test.php
+./vendor/bin/phpunit --testdox --bootstrap vendor/autoload.php tests.php
