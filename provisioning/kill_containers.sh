f82b04facc05f5c328287756ebb8f3b01c10d251
Leon Stafford <leon.stafford@mac.com>
2018-08-24 04:48:00 +1000

    update provisioning utility
    Former-commit-id: ea605fd9ff36298a72cd584e05ec9341fd950086

--- provisioning/kill_containers.sh
+++ provisioning/kill_containers.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 # convenience script to destroy any running containers
-sudo docker rm -f devmysql
-sudo docker rm -f plugindevwp
-sudo docker rm -f seleniumserver
+sudo docker rm -f {devmysql,plugindevwp,phpmyadmin,ftpserver}
+sudo docker network rm devwp
