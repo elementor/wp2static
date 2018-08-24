4f054e5fd4e400fd1d8d7ebef28f1188e3a88c9d
Leon Stafford <leon.stafford@mac.com>
2018-08-24 17:56:57 +1000

    preparing files for 5.5
    Former-commit-id: 9d947d033de6cd2e3b9e78d1de1ea082ded4b075

--- library/StaticHtmlOutput.php
+++ library/StaticHtmlOutput.php
@@ -6,7 +6,7 @@
  */
 
 class StaticHtmlOutput_Controller {
-	const VERSION = '5.4.2';
+	const VERSION = '5.5';
 	const OPTIONS_KEY = 'wp-static-html-output-options';
 	const HOOK = 'wp-static-html-output';
 
