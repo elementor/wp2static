df09ec1d64cc070f9d41a54edb18485c394fed13
Leon Stafford <leon.stafford@mac.com>
2018-08-25 20:45:13 +1000

    retab

--- library/StaticHtmlOutput/WsLog.php
+++ library/StaticHtmlOutput/WsLog.php
@@ -7,22 +7,22 @@
 
 class WsLog
 {
-    public static function l($text, $verbose = false) {
-        $tmp_var_to_hold_return_array = wp_upload_dir();
-		$file = $tmp_var_to_hold_return_array['basedir'] . '/WP-STATIC-EXPORT-LOG';
+  public static function l($text, $verbose = false) {
+    $tmp_var_to_hold_return_array = wp_upload_dir();
+    $file = $tmp_var_to_hold_return_array['basedir'] . '/WP-STATIC-EXPORT-LOG';
 
-		// TODO: add flag for verbose log or normal, write to -LOG-VERBOSE
-       
-        $src = fopen($file, (file_exists($file)) ? 'r+' : 'w');
-        $dest = fopen('php://temp', 'w');
+    // TODO: add flag for verbose log or normal, write to -LOG-VERBOSE
 
-        fwrite($dest,  date("Y-m-d h:i:s") . ' ' . $text . PHP_EOL);
-        stream_copy_to_stream($src, $dest);
-        rewind($dest);
-        rewind($src);
-        stream_copy_to_stream($dest, $src);
-        fclose($src);
-        fclose($dest);
-    }
+    $src = fopen($file, (file_exists($file)) ? 'r+' : 'w');
+    $dest = fopen('php://temp', 'w');
+
+    fwrite($dest,  date("Y-m-d h:i:s") . ' ' . $text . PHP_EOL);
+    stream_copy_to_stream($src, $dest);
+    rewind($dest);
+    rewind($src);
+    stream_copy_to_stream($dest, $src);
+    fclose($src);
+    fclose($dest);
+  }
 
 }
