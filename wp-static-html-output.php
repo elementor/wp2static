f30ccc558cb50efdfed57efc7aee0a127713302d
Leon Stafford <leon.stafford@mac.com>
2018-09-07 11:33:06 +1000

    fix for #150 - allow 5.3 users to activate with warning

--- wp-static-html-output.php
+++ wp-static-html-output.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Static Site Generator
  * Plugin URI:  https://wp2static.com
  * Description: The optimum solution to speed up and secure your WordPress site - export to static HTML and hide all traces of WordPress from your site!
- * Version:     5.7
+ * Version:     5.8
  * Author:      Leon Stafford
  * Author URI:  https://leonstafford.github.io
  * Text Domain: static-html-output-plugin
