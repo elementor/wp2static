457749f54cb1d5277254853445e6147a0a88ae0d
Leon Stafford <leon.stafford@mac.com>
2018-09-17 22:19:39 +1000

    wip CSS parser manual require
    Former-commit-id: 970933ce3c549621d64c89dfd6b1f2a4f1127710

--- library/StaticHtmlOutput/CSSProcessor.php
+++ library/StaticHtmlOutput/CSSProcessor.php
@@ -3,8 +3,51 @@
 
 class CSSProcessor {
 
-  public function __construct($html_document){
+  public function __construct($css_document){
     error_log('instantiating CSS Processor');
+    
+    // parse CSS into easily modifiable form
+    require_once dirname(__FILE__) . '/../CSSParser/Parser.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Settings.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Renderable.php';
+    require_once dirname(__FILE__) . '/../CSSParser/OutputFormat.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Comment/Comment.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Comment/Commentable.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Parsing/SourceException.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Parsing/OutputException.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Parsing/UnexpectedTokenException.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Property/AtRule.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Property/Charset.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Property/CSSNamespace.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Property/Import.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Property/Selector.php';
+    require_once dirname(__FILE__) . '/../CSSParser/RuleSet/RuleSet.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Rule/Rule.php';
+    require_once dirname(__FILE__) . '/../CSSParser/RuleSet/AtRuleSet.php';
+    require_once dirname(__FILE__) . '/../CSSParser/RuleSet/DeclarationBlock.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Value/Value.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Value/ValueList.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Value/RuleValueList.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Value/CSSFunction.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Value/CalcFunction.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Value/CalcRuleValueList.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Value/PrimitiveValue.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Value/Color.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Value/CSSString.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Value/LineName.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Value/Size.php';
+    require_once dirname(__FILE__) . '/../CSSParser/Value/URL.php';
+    require_once dirname(__FILE__) . '/../CSSParser/CSSList/CSSList.php';
+    require_once dirname(__FILE__) . '/../CSSParser/CSSList/CSSBlockList.php';
+    require_once dirname(__FILE__) . '/../CSSParser/CSSList/AtRuleBlockList.php';
+    require_once dirname(__FILE__) . '/../CSSParser/CSSList/Document.php';
+    require_once dirname(__FILE__) . '/../CSSParser/CSSList/KeyFrame.php';
+
+    $oCssParser = new Sabberworm\CSS\Parser(file_get_contents($css_document));
+    $oCssDocument = $oCssParser->parse();
+
+
+
   }
 
 	public function cleanup($wp_site_environment, $overwrite_slug_targets) {
