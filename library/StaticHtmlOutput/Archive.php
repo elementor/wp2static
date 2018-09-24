bc84a5c0b55b58fcb72d311acde55958fd6dd88e
Leon Stafford <leon.stafford@mac.com>
2018-09-24 21:53:19 +1000

    create archive dir/name in export prep
    Former-commit-id: 76caa3fc59f5b2c0157715a1cd32153a2f69537e

--- library/StaticHtmlOutput/Archive.php
+++ library/StaticHtmlOutput/Archive.php
@@ -6,12 +6,23 @@ class Archive {
     $this->path = '';
     $this->crawl_list = '';
     $this->export_log = '';
-    $this-> = '';
+    $this->working_directory = isset($_POST['working_directory']) ? $_POST['working_directory'] : '';
   }
 
   public function addFiles() {
 
   }
 
+  public function create() {
+		$this->archive_name = $this->working_directory . '/wp-static-html-output-' . time();
 
+		$this->archive_directory = $this->archive_name . '/';
+
+    if (wp_mkdir_p($this->archive_directory)) {
+      // saving the current archive name to file to persist across requests / functions
+      file_put_contents($this->working_directory . '/WP-STATIC-CURRENT-ARCHIVE', $this->archive_directory);
+    } else {
+      error_log("Couldn't create the archive directory at " . $this->archive_directory);
+    }
+  }
 }
