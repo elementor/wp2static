0a356bc7dffe25491aefbb4a156a29a1440cd3fb
Gray Arswright <gray@arswright.com>
2018-09-24 02:44:45 -0700

    utility script bugfix
    Former-commit-id: 6649eff597d6c23e8b1a6f81c9b15c6d345cbdb3

--- todo.sh
+++ todo.sh
@@ -6,6 +6,6 @@ FILE_TARGET="$( cat todo.list | head -n1 )"
 echo "" >> "$FILE_TARGET"
 git add .
 git commit -a
-echo "$FILE_TARGET" > todo-done.list
+echo "$FILE_TARGET" >> todo-done.list
 tail -n +2 todo.list > todo.list.tmp
 mv todo.list.tmp todo.list
