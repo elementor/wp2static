05a1f51fed0743b44b3d2a70464b9c3007e07903
Gray Arswright <gray@arswright.com>
2018-09-24 02:48:46 -0700

    code cleanup

--- library/Github/clue/stream-filter/examples/uppercase.php
+++ library/Github/clue/stream-filter/examples/uppercase.php
@@ -1,9 +1,14 @@
 <?php
+/**
+ * Uppercase
+ *
+ * @package WP2Static
+ */
 
 // $ echo test | php examples/uppercase.php
-
 require __DIR__ . '/../vendor/autoload.php';
 
-Clue\StreamFilter\append(STDIN, 'strtoupper');
+Clue\StreamFilter\append( STDIN, 'strtoupper' );
 
-fpassthru(STDIN);
+fpassthru( STDIN );
+
