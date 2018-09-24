6a61de50e0d17c572c3aea18b04399d7deec351e
Gray Arswright <gray@arswright.com>
2018-09-24 02:39:19 -0700

    add utility script and supporting files (todo: delete from branch at completion)

---
+++ todo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# temporary maintenance script
+#
+FILE_TARGET="$( cat todo.list | head -n1 )"
+echo "" >> "$FILE_TARGET"
+git add .
+git commit -a
+echo "$FILE_TARGET" > todo-done.list
+tail -n +2 todo.list > todo.list.tmp
+mv todo.list.tmp todo.list
