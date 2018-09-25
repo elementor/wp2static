c588702cf768ba8096a9950e733eb200dca3687c
Leon Stafford <leon.stafford@mac.com>
2018-09-25 18:17:30 +1000

    faster file writing within logging;within working dir

--- library/StaticHtmlOutput/WsLog.php
+++ library/StaticHtmlOutput/WsLog.php
@@ -8,18 +8,8 @@ class WsLog {
 
     $log_file_path = $working_directory . '/WP-STATIC-EXPORT-LOG';
 
-    // TODO: create file when initializing the log with header information, reducing each call here
-
-
-    $src = fopen($log_file_path, (file_exists($log_file_path)) ? 'r+' : 'w');
-    $dest = fopen('php://temp', 'w');
-
-    fwrite($dest,  date("Y-m-d h:i:s") . ' ' . $text . PHP_EOL);
-    stream_copy_to_stream($src, $dest);
-    rewind($dest);
-    rewind($src);
-    stream_copy_to_stream($dest, $src);
-    fclose($src);
-    fclose($dest);
+    $myfile = fopen($log_file_path, "a") or die("Unable to open file!");
+    fwrite($myfile, $text . PHP_EOL);
+    fclose($myfile);
   }
 }
