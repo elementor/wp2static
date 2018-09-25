5cab4978af4ac689ff538389846d6e0beda0a0c9
Gray Arswright <gray@arswright.com>
2018-09-25 11:23:02 -0700

    code cleanup
    Former-commit-id: 9d0cebd308f3b93c8614a30d8b0ddcddf980b559

--- provisioning/tests/Ubench.php
+++ provisioning/tests/Ubench.php
@@ -3,11 +3,11 @@
  * Ubench
  *
  * @package WP2Static
+ *
+ * https://github.com/devster/ubench
  */
+class Ubench {
 
-// https://github.com/devster/ubench
-class Ubench
-{
     protected $start_time;
 
     protected $end_time;
@@ -19,77 +19,73 @@ class Ubench
      *
      * @return void
      */
-    public function start()
-    {
-        $this->start_time = microtime(true);
+    public function start() {
+        $this->start_time = microtime( true );
     }
 
     /**
      * Sets end microtime
      *
      * @return void
-     * @throws Exception
+     * @throws LogicException If not started
      */
-    public function end()
-    {
-        if (!$this->hasStarted())
-        {
-            throw new LogicException("You must call start()");
+    public function end() {
+        if ( ! $this->hasStarted() ) {
+            throw new LogicException( 'You must call start()' );
         }
 
-        $this->end_time = microtime(true);
-        $this->memory_usage = memory_get_usage(true);
+        $this->end_time = microtime( true );
+        $this->memory_usage = memory_get_usage( true );
     }
 
     /**
      * Returns the elapsed time, readable or not
      *
-     * @param bool $raw
-     * @param  string $format The format to display (printf format)
+     * @param boolean $raw    Raw output flag
+     * @param string  $format Display format (printf)
      * @return float|string
-     * @throws Exception
+     * @throws LogicException If not started/ended
      */
-    public function getTime($raw = false, $format = null)
-    {
-        if (!$this->hasStarted())
-        {
-            throw new LogicException("You must call start()");
+    public function getTime( $raw = false, $format = null ) {
+        if ( ! $this->hasStarted() ) {
+            throw new LogicException( 'You must call start()' );
         }
 
-        if (!$this->hasEnded())
-        {
-            throw new LogicException("You must call end()");
+        if ( ! $this->hasEnded() ) {
+            throw new LogicException( 'You must call end()' );
         }
 
         $elapsed = $this->end_time - $this->start_time;
 
-        return $raw ? $elapsed : self::readableElapsedTime($elapsed, $format);
+        return $raw ? $elapsed : self::readableElapsedTime( $elapsed, $format );
     }
 
     /**
      * Returns the memory usage at the end checkpoint
      *
-     * @param  boolean $readable Whether the result must be human readable
-     * @param  string  $format   The format to display (printf format)
+     * @param  boolean $raw    Raw output flag
+     * @param  string  $format Display format (printf)
      * @return string|float
      */
-    public function getMemoryUsage($raw = false, $format = null)
-    {
-        return $raw ? $this->memory_usage : self::readableSize($this->memory_usage, $format);
+    public function getMemoryUsage( $raw = false, $format = null ) {
+        return $raw
+            ? $this->memory_usage
+            : self::readableSize( $this->memory_usage, $format );
     }
 
     /**
      * Returns the memory peak, readable or not
      *
-     * @param  boolean $readable Whether the result must be human readable
-     * @param  string  $format   The format to display (printf format)
+     * @param  boolean $raw    Raw result flag
+     * @param  string  $format Display format (printf)
      * @return string|float
      */
-    public function getMemoryPeak($raw = false, $format = null)
-    {
-        $memory = memory_get_peak_usage(true);
+    public function getMemoryPeak( $raw = false, $format = null ) {
+        $memory = memory_get_peak_usage( true );
 
-        return $raw ? $memory : self::readableSize($memory, $format);
+        return $raw
+            ? $memory
+            : self::readableSize( $memory, $format );
     }
 
     /**
@@ -98,16 +94,15 @@ class Ubench
      * Additional arguments passed to this method will be passed to
      * the callable.
      *
-     * @param callable $callable
+     * @param callable $callable Function to call
      * @return mixed
      */
-    public function run(callable $callable)
-    {
+    public function run( callable $callable ) {
         $arguments = func_get_args();
-        array_shift($arguments);
+        array_shift( $arguments );
 
         $this->start();
-        $result = call_user_func_array($callable, $arguments);
+        $result = call_user_func_array( $callable, $arguments );
         $this->end();
 
         return $result;
@@ -116,66 +111,81 @@ class Ubench
     /**
      * Returns a human readable memory size
      *
-     * @param   int    $size
-     * @param   string $format   The format to display (printf format)
-     * @param   int    $round
+     * @param   int    $size   Size
+     * @param   string $format Display format (printf)
+     * @param   int    $round  Rounding precision
      * @return  string
      */
-    public static function readableSize($size, $format = null, $round = 3)
-    {
+    public static function readableSize( $size, $format = null, $round = 3 ) {
         $mod = 1024;
 
-        if (is_null($format)) {
+        if ( is_null( $format ) ) {
             $format = '%.2f%s';
         }
 
-        $units = explode(' ','B Kb Mb Gb Tb');
+        $units = explode( ' ', 'B Kb Mb Gb Tb' );
 
-        for ($i = 0; $size > $mod; $i++) {
+        for ( $i = 0; $size > $mod; $i++ ) {
             $size /= $mod;
         }
 
-        if (0 === $i) {
-            $format = preg_replace('/(%.[\d]+f)/', '%d', $format);
+        if ( 0 === $i ) {
+            $format = preg_replace( '/(%.[\d]+f)/', '%d', $format );
         }
 
-        return sprintf($format, round($size, $round), $units[$i]);
+        return sprintf( $format, round( $size, $round ), $units[ $i ] );
     }
 
     /**
      * Returns a human readable elapsed time
      *
-     * @param  float $microtime
-     * @param  string  $format   The format to display (printf format)
+     * @param  float   $microtime Microtime
+     * @param  string  $format    Display format (printf)
+     * @param  integer $round     Rounding precision
      * @return string
      */
-    public static function readableElapsedTime($microtime, $format = null, $round = 3)
-    {
-        if (is_null($format)) {
+    public static function readableElapsedTime(
+        $microtime,
+        $format = null,
+        $round = 3
+    ) {
+        if ( is_null( $format ) ) {
             $format = '%.3f%s';
         }
 
-        if ($microtime >= 1) {
+        if ( $microtime >= 1 ) {
             $unit = 's';
-            $time = round($microtime, $round);
+            $time = round( $microtime, $round );
         } else {
             $unit = 'ms';
-            $time = round($microtime*1000);
+            $time = round( $microtime * 1000 );
 
-            $format = preg_replace('/(%.[\d]+f)/', '%d', $format);
+            $format = preg_replace( '/(%.[\d]+f)/', '%d', $format );
         }
 
-        return sprintf($format, $time, $unit);
+        return sprintf( $format, $time, $unit );
     }
 
-    public function hasEnded()
-    {
-        return isset($this->end_time);
+
+    /**
+     * Check whether ended
+     *
+     * @return boolean
+     */
+    public function hasEnded() {
+        return isset( $this->end_time );
     }
 
-    public function hasStarted()
-    {
-        return isset($this->start_time);
+
+    /**
+     * Check whether started
+     *
+     * @return boolean
+     */
+    public function hasStarted() {
+        return isset( $this->start_time );
     }
+
 }
+
 
