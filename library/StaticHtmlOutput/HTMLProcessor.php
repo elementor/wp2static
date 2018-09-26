280a6a640bb6d33e4c67ef813fe284a6bffb729a
Leon Stafford <leon.stafford@mac.com>
2018-09-26 21:33:47 +1000

    syntax cleanup
    Former-commit-id: dd80294904402afc86a344fcbad6d4d1cbeee417

--- library/StaticHtmlOutput/HTMLProcessor.php
+++ library/StaticHtmlOutput/HTMLProcessor.php
@@ -19,10 +19,13 @@ class HTMLProcessor {
 
         if ( $this->discoverNewURLs ) {
             $this->discovered_urls = [];
-            //$this->excludes_list = [];
-            //$this->includes_list = [];
+            // $this->excludes_list = [];
+            // $this->includes_list = [];
             $this->wp_uploads_path = $_POST['wp_uploads_path'];
-            $this->working_directory = isset($_POST['workingDirectory']) ? $_POST['workingDirectory'] : $this->wp_uploads_path;
+            $this->working_directory =
+                isset( $_POST['workingDirectory'] ) ?
+                $_POST['workingDirectory'] :
+                $this->wp_uploads_path;
         }
 
         // PERF: 70% of function time
@@ -35,8 +38,9 @@ class HTMLProcessor {
     public function writeDiscoveredURLs() {
         file_put_contents(
             $this->working_directory . '/WP-STATIC-DISCOVERED-URLS',
-            implode(PHP_EOL, array_unique($this->discovered_urls)),
-            FILE_APPEND | LOCK_EX);
+            implode( PHP_EOL, array_unique( $this->discovered_urls ) ),
+            FILE_APPEND | LOCK_EX
+        );
     }
 
     // make all links absolute and relative to the current document
@@ -50,26 +54,31 @@ class HTMLProcessor {
             if ( $this->isInternalLink( $original_link ) ) {
                 $abs = $base->resolve( $original_link );
                 $link->setAttribute( 'href', $abs );
-            
+
                 $this->discovered_urls[] = $abs;
             }
         }
     }
 
-    public function cleanup( $wp_site_environment, $overwrite_slug_targets ) {
+    public function cleanup( $wp_site_env, $new_site_paths ) {
         $this->stripWPMetaElements();
         $this->stripWPLinkElements();
         $this->removeQueryStringsFromInternalLinks();
-        $this->rewriteWPPaths( $wp_site_environment, $overwrite_slug_targets );
-        $this->detectEscapedSiteURLs( $wp_site_environment, $overwrite_slug_targets );
+        $this->rewriteWPPaths( $wp_site_env, $new_site_paths );
+        $this->detectEscapedSiteURLs(
+            $wp_site_env,
+            $new_site_paths
+        );
     }
 
     public function isInternalLink( $link ) {
-        // TODO: apply only to links starting with .,..,/, 
+        // TODO: apply only to links starting with .,..,/,
         // or any with just a path, like banana.png
-
         // check link is same host as $this->url and not a subdomain
-        return parse_url( $link, PHP_URL_HOST ) === parse_url( $this->wp_site_url, PHP_URL_HOST );
+        return parse_url( $link, PHP_URL_HOST ) === parse_url(
+            $this->wp_site_url,
+            PHP_URL_HOST
+        );
     }
 
     public function removeQueryStringsFromInternalLinks() {
@@ -109,7 +118,9 @@ class HTMLProcessor {
     }
 
     public function stripWPMetaElements() {
-        $metas = iterator_to_array( $this->xml_doc->getElementsByTagName( 'meta' ) );
+        $metas = iterator_to_array(
+            $this->xml_doc->getElementsByTagName( 'meta' )
+        );
 
         foreach ( $metas as $meta ) {
             $meta_name = $meta->getAttribute( 'name' );
@@ -135,7 +146,9 @@ class HTMLProcessor {
             'wlwmanifest',
         );
 
-        $links = iterator_to_array( $this->xml_doc->getElementsByTagName( 'link' ) );
+        $links = iterator_to_array(
+            $this->xml_doc->getElementsByTagName( 'link' )
+        );
 
         foreach ( $links as $link ) {
             $link_rel = $link->getAttribute( 'rel' );
@@ -147,45 +160,54 @@ class HTMLProcessor {
         }
     }
 
-    public function detectEscapedSiteURLs( $wp_site_environment, $overwrite_slug_targets ) {
+    public function detectEscapedSiteURLs(
+        $wp_site_env,
+        $new_site_paths
+    ) {
         // NOTE: this does return the expected http:\/\/172.18.0.3
-        // but the PHP error log will escape again and show http:\\/\\/172.18.0.3
+        // but the PHP error log will escape again and
+        // show http:\\/\\/172.18.0.3
         $escaped_site_url = addcslashes( get_option( 'siteurl' ), '/' );
 
-        if ( strpos( $this->raw_html, $escaped_site_url ) !== false ) {
-            // TODO: renable this function being called. needs to be on raw HTML, so ideally after the Processor has done all other XML things, so no need to parse again
-            // suggest adding a processRawHTML function, that includes this stuff.. and call it within the getHTML function or finalizeProcessing or such....
-            // error_log('skipping rewriting of escaped URLs');// leave this error log in until fixed
-            // $this->rewriteEscapedURLs($wp_site_environment, $overwrite_slug_targets);
-        }
+        // if ( strpos( $this->raw_html, $escaped_site_url ) !== false ) {
+        // TODO: renable this function being called. needs to be on
+        // raw HTML, so ideally after the Processor has done all other
+        // XML things, so no need to parse again
+        // suggest adding a processRawHTML function, that
+        // includes this stuff.. and call it within the getHTML function
+        // or finalizeProcessing or such....
+        // $this->rewriteEscapedURLs($wp_site_env,
+        // $new_site_paths);
+        // }
     }
 
-    public function rewriteEscapedURLs( $wp_site_environment, $overwrite_slug_targets ) {
+    public function rewriteEscapedURLs(
+        $wp_site_env,
+        $new_site_paths ) {
         /*
-        this function will be a bit more costly. To cover bases like:
+        This function will be a bit more costly. To cover bases like:
 
-        <section  id="hero"  data-images="[&quot;https:\/\/mysite.example.com\/wp-content\/themes\/onepress\/assets\/images\/hero5.jpg&quot;]"             class="hero-slideshow-wrapper hero-slideshow-normal">
-
+         data-images="[&quot;https:\/\/mysite.example.com\/wp...
         from the onepress(?) theme, for example
 
         */
 
         $rewritten_source = str_replace(
             array(
-                addcslashes( $wp_site_environment['wp_active_theme'], '/' ),
-                addcslashes( $wp_site_environment['wp_themes'], '/' ),
-                addcslashes( $wp_site_environment['wp_uploads'], '/' ),
-                addcslashes( $wp_site_environment['wp_plugins'], '/' ),
-                addcslashes( $wp_site_environment['wp_content'], '/' ),
-                addcslashes( $wp_site_environment['wp_inc'], '/' ),
+                addcslashes( $wp_site_env['wp_active_theme'], '/' ),
+                addcslashes( $wp_site_env['wp_themes'], '/' ),
+                addcslashes( $wp_site_env['wp_uploads'], '/' ),
+                addcslashes( $wp_site_env['wp_plugins'], '/' ),
+                addcslashes( $wp_site_env['wp_content'], '/' ),
+                addcslashes( $wp_site_env['wp_inc'], '/' ),
             ),
             array(
-                addcslashes( $overwrite_slug_targets['new_active_theme_path'], '/' ),
-                addcslashes( $overwrite_slug_targets['new_themes_path'], '/' ),
-                addcslashes( $overwrite_slug_targets['new_uploads_path'], '/' ),
-                addcslashes( $overwrite_slug_targets['new_plugins_path'], '/' ),
-                addcslashes( $overwrite_slug_targets['new_wp_content_path'], '/' ),
-                addcslashes( $overwrite_slug_targets['new_wpinc_path'], '/' ),
+                addcslashes( $new_site_paths['new_active_theme_path'], '/' ),
+                addcslashes( $new_site_paths['new_themes_path'], '/' ),
+                addcslashes( $new_site_paths['new_uploads_path'], '/' ),
+                addcslashes( $new_site_paths['new_plugins_path'], '/' ),
+                addcslashes( $new_site_paths['new_wp_content_path'], '/' ),
+                addcslashes( $new_site_paths['new_wpinc_path'], '/' ),
             ),
             $this->response['body']
         );
@@ -193,7 +215,7 @@ class HTMLProcessor {
         $this->setResponseBody( $rewritten_source );
     }
 
-    public function rewriteWPPaths( $wp_site_environment, $overwrite_slug_targets ) {
+    public function rewriteWPPaths( $wp_site_env, $new_site_paths ) {
         // NOTE: drier code but costlier memory usage
         foreach ( $this->xml_doc->getElementsByTagName( '*' ) as $element ) {
             $attribute_to_change = '';
@@ -211,23 +233,23 @@ class HTMLProcessor {
             $url_to_change = $element->getAttribute( $attribute_to_change );
 
             if ( $this->isInternalLink( $url_to_change ) ) {
-                // rewrite all the things, starting with longest paths down to shortest
+                // rewrite URLs, starting with longest paths down to shortest
                 $rewritten_url = str_replace(
                     array(
-                        $wp_site_environment['wp_active_theme'],
-                        $wp_site_environment['wp_themes'],
-                        $wp_site_environment['wp_uploads'],
-                        $wp_site_environment['wp_plugins'],
-                        $wp_site_environment['wp_content'],
-                        $wp_site_environment['wp_inc'],
+                        $wp_site_env['wp_active_theme'],
+                        $wp_site_env['wp_themes'],
+                        $wp_site_env['wp_uploads'],
+                        $wp_site_env['wp_plugins'],
+                        $wp_site_env['wp_content'],
+                        $wp_site_env['wp_inc'],
                     ),
                     array(
-                        $overwrite_slug_targets['new_active_theme_path'],
-                        $overwrite_slug_targets['new_themes_path'],
-                        $overwrite_slug_targets['new_uploads_path'],
-                        $overwrite_slug_targets['new_plugins_path'],
-                        $overwrite_slug_targets['new_wp_content_path'],
-                        $overwrite_slug_targets['new_wpinc_path'],
+                        $new_site_paths['new_active_theme_path'],
+                        $new_site_paths['new_themes_path'],
+                        $new_site_paths['new_uploads_path'],
+                        $new_site_paths['new_plugins_path'],
+                        $new_site_paths['new_wp_content_path'],
+                        $new_site_paths['new_wpinc_path'],
                     ),
                     $url_to_change
                 );
@@ -243,11 +265,17 @@ class HTMLProcessor {
     }
 
     // TODO: damn, this is going to take a while to cleanup...
-    // TODO: some optimization to be gained by doing this in same loop as rewriteWPPaths loop
-    public function replaceBaseUrl( $oldBaseUrl, $newBaseUrl, $allowOfflineUsage, $absolutePaths = false, $useBaseHref = true ) {
+    // TODO: some optimization to be gained by doing this in same
+    // loop as rewriteWPPaths loop
+    public function replaceBaseUrl(
+        $old_URL,
+        $new_URL,
+        $allowOfflineUsage,
+        $absolutePaths = false,
+        $useBaseHref = true ) {
 
-        // error_log('old base url: ' . $oldBaseUrl);
-        // error_log('new base url: ' . $newBaseUrl);
+        // error_log('old base url: ' . $old_URL);
+        // error_log('new base url: ' . $new_URL);
         foreach ( $this->xml_doc->getElementsByTagName( '*' ) as $element ) {
             if ( $element->hasAttribute( 'href' ) ) {
                 $attribute_to_change = 'href';
@@ -261,8 +289,12 @@ class HTMLProcessor {
             $url_to_change = $element->getAttribute( $attribute_to_change );
 
             // check it actually needs to be changed
-            if ( strpos( $url_to_change, $oldBaseUrl ) !== false ) {
-                $rewritten_url = str_replace( $oldBaseUrl, $newBaseUrl, $url_to_change );
+            if ( strpos( $url_to_change, $old_URL ) !== false ) {
+                $rewritten_url = str_replace(
+                    $old_URL,
+                    $new_URL,
+                    $url_to_change
+                );
 
                 $element->setAttribute( $attribute_to_change, $rewritten_url );
             }
@@ -270,31 +302,44 @@ class HTMLProcessor {
 
         if ( $absolutePaths ) {
 
-            // TODO: re-implement as separate function as another processing layer
+            // TODO: re-implement as separate func as another processing layer
             // error_log('SKIPPING absolute path rewriting');
             if ( $useBaseHref ) {
-                $responseBody = str_replace( '<head>', "<head>\n<base href=\"" . esc_attr( $newBaseUrl ) . "/\" />\n", $responseBody );
+                $responseBody = str_replace(
+                    '<head>',
+                    "<head>\n<base href=\"" .
+                    esc_attr( $new_URL ) . "/\" />\n",
+                    $responseBody
+                );
             } else {
-                $responseBody = str_replace( '<head>', "<head>\n<base href=\"/\" />\n", $responseBody );
+                $responseBody = str_replace(
+                    '<head>',
+                    "<head>\n<base href=\"/\" />\n",
+                    $responseBody
+                );
             }
-        } elseif ( $allowOfflineUsage ) {
-            // detect urls starting with our domain and append index.html to the end if they end in /
-            // TODO: re-implement as separate function as another processing layer
-            // error_log('SKIPPING offline usage rewriting');
-            // foreach($xml->getElementsByTagName('a') as $link) {
-            // $original_link = $link->getAttribute("href");
-            //
-            // process links from our site only
-            // if (strpos($original_link, $oldDomain) !== false) {
-            //
-            // }
-            //
-            // $link->setAttribute('href', $original_link . 'index.html');
-            // }
-            //
         }
 
-        // TODO: should we check for incorrectly linked references? like an http link on a WP site served from https? - probably not
+        // elseif ( $allowOfflineUsage ) {
+        // detect urls starting with our domain and append index.html to
+        // the end if they end in /
+        // TODO: re-implement as separate function as
+        // another processing layer
+        // error_log('SKIPPING offline usage rewriting');
+        // foreach($xml->getElementsByTagName('a') as $link) {
+        // $original_link = $link->getAttribute("href");
+        //
+        // process links from our site only
+        // if (strpos($original_link, $oldDomain) !== false) {
+        //
+        // }
+        //
+        // $link->setAttribute('href', $original_link . 'index.html');
+        // }
+        //
+        // }
+        // TODO: should we check for incorrectly linked references?
+        // like an http link on a WP site served from https? - probably not
     }
 }
 
