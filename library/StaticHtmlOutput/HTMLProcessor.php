61701dfd96ebfdbe2b064cc650a899abf18d410a
Leon Stafford <leon.stafford@mac.com>
2018-09-27 20:50:09 +1000

    wip per element processing for HTML

--- library/StaticHtmlOutput/HTMLProcessor.php
+++ library/StaticHtmlOutput/HTMLProcessor.php
@@ -71,22 +71,24 @@ class HTMLProcessor {
                     $this->processMeta($element);
                     break;
                 case 'a':
-                    die();
+                    $this->processAnchor($element);
+                    break;
+                case 'img':
+                    $this->processImage($element);
+                    break;
+                case 'link':
+                    // NOTE: not to confuse with anchor element
                     $this->processLink($element);
                     break;
 
             }
         }
 
+        $this->removeQueryStringsFromInternalLinks();
+        $this->rewriteWPPaths();
 
-        if ( $this->discoverNewURLs && $_POST['ajax_action'] === 'crawl_site' ) {
-            $processor->writeDiscoveredURLs();
-        }
-
-        $processor->cleanup(
-            $wp_site_environment,
-            $overwrite_slug_targets
-        );
+        // funcs to apply to whole page
+        $this->detectEscapedSiteURLs();
 
         $processor->replaceBaseUrl(
             $this->wp_site_url,
@@ -95,15 +97,57 @@ class HTMLProcessor {
             $this->useRelativeURLs,
             $this->useBaseHref
         );
+
+        // TODO: this whole 1-layer only discovery needs to be redone
+        //       was a quick add-on to get some real crawling in 5.9 
+        if ( $this->discoverNewURLs && $_POST['ajax_action'] === 'crawl_site' ) {
+            $processor->writeDiscoveredURLs();
+        }
     }
 
+    public function processLink( $element ) {
+        $relativeLinksToRemove = array(
+            'shortlink',
+            'canonical',
+            'pingback',
+            'alternate',
+            'EditURI',
+            'wlwmanifest',
+            'index',
+            'profile',
+            'prev',
+            'next',
+            'wlwmanifest',
+        );
 
+        $link_rel = $element->getAttribute( 'rel' );
+
+        if ( in_array( $link_rel, $relativeLinksToRemove ) ) {
+            $element->parentNode->removeChild( $element );
+        } elseif ( strpos( $link_rel, '.w.org' ) !== false ) {
+            $element->parentNode->removeChild( $element );
+        }
+    }
+
+    public function addDiscoveredURL( $url ) {
+        if ( $this->discoverNewURLs ) {
+            $this->discovered_urls[] = $url;
+        }
+    }
+
+    public function processImage( $element ) {
+        $this->normalizeURL( $element, 'src' );
+        $this->discovered_urls[] = $element->getAttribute( 'src' );
+        $this->rewriteWPPaths( $element );
+        $this->rewriteBaseURL( $element );
+        
+    }
 
     public function processAnchor( $element ) {
         $this->normalizeURL( $element, 'href' );
+        $this->discovered_urls[] = $element->getAttribute( 'href' );
         $this->rewriteWPPaths( $element );
         $this->rewriteBaseURL( $element );
-
         
     }
 
@@ -116,6 +160,11 @@ class HTMLProcessor {
     }
 
     public function writeDiscoveredURLs() {
+        if ( ! $this->discoverNewURLs &&
+            $_POST['ajax_action'] === 'crawl_site' ) {
+            return;
+        }
+
         file_put_contents(
             $this->working_directory . '/WP-STATIC-DISCOVERED-URLS',
             PHP_EOL .
@@ -124,30 +173,17 @@ class HTMLProcessor {
         );
     }
 
-    public function addDiscoveredURL( $url ) {
-        if ( $this->discoverNewURLs ) {
-            $this->discovered_urls[] = $url;
-        }
-    }
-
     // make all links absolute and relative to the current document
     public function normalizeURL($element, $attribute) {
-        $original_link = $link->getAttribute( $attribute );
+        $original_link = $element->getAttribute( $attribute );
 
         if ( $this->isInternalLink( $original_link ) ) {
             $abs = $this->page_url->resolve( $original_link );
             $element->setAttribute( $attribute, $abs );
-
-            $this->addDiscoveredURL( $abs );
         }
     }
 
     public function cleanup() {
-        $this->stripWPMetaElements();
-        $this->stripWPLinkElements();
-        $this->removeQueryStringsFromInternalLinks();
-        $this->rewriteWPPaths();
-        $this->detectEscapedSiteURLs();
     }
 
     public function isInternalLink( $link ) {
@@ -196,48 +232,6 @@ class HTMLProcessor {
         }
     }
 
-    public function stripWPMetaElements() {
-        $metas = iterator_to_array(
-            $this->xml_doc->getElementsByTagName( 'meta' )
-        );
-
-        foreach ( $metas as $meta ) {
-            $meta_name = $meta->getAttribute( 'name' );
-
-            if ( strpos( $meta_name, 'generator' ) !== false ) {
-                $meta->parentNode->removeChild( $meta );
-            }
-        }
-    }
-
-    public function stripWPLinkElements() {
-        $relativeLinksToRemove = array(
-            'shortlink',
-            'canonical',
-            'pingback',
-            'alternate',
-            'EditURI',
-            'wlwmanifest',
-            'index',
-            'profile',
-            'prev',
-            'next',
-            'wlwmanifest',
-        );
-
-        $links = iterator_to_array(
-            $this->xml_doc->getElementsByTagName( 'link' )
-        );
-
-        foreach ( $links as $link ) {
-            $link_rel = $link->getAttribute( 'rel' );
-            if ( in_array( $link_rel, $relativeLinksToRemove ) ) {
-                $link->parentNode->removeChild( $link );
-            } elseif ( strpos( $link_rel, '.w.org' ) !== false ) {
-                $link->parentNode->removeChild( $link );
-            }
-        }
-    }
 
     public function detectEscapedSiteURLs() {
         // NOTE: this does return the expected http:\/\/172.18.0.3
@@ -289,97 +283,81 @@ class HTMLProcessor {
         $this->setResponseBody( $rewritten_source );
     }
 
-    public function rewriteWPPaths( $wp_site_env, $new_site_paths ) {
-        // NOTE: drier code but costlier memory usage
-        foreach ( $this->xml_doc->getElementsByTagName( '*' ) as $element ) {
-            $attribute_to_change = '';
-            $url_to_change = '';
+    public function rewriteWPPaths( $element ) {
+        $attribute_to_change = '';
+        $url_to_change = '';
 
-            if ( $element->hasAttribute( 'href' ) ) {
-                $attribute_to_change = 'href';
-            } elseif ( $element->hasAttribute( 'src' ) ) {
-                $attribute_to_change = 'src';
-                // skip elements without href or src
-            } else {
-                continue;
-            }
+        if ( $element->hasAttribute( 'href' ) ) {
+            $attribute_to_change = 'href';
+        } elseif ( $element->hasAttribute( 'src' ) ) {
+            $attribute_to_change = 'src';
+            // skip elements without href or src
+        } else {
+            return;
+        }
 
-            $url_to_change = $element->getAttribute( $attribute_to_change );
+        $url_to_change = $element->getAttribute( $attribute_to_change );
 
-            if ( $this->isInternalLink( $url_to_change ) ) {
-                // rewrite URLs, starting with longest paths down to shortest
-                $rewritten_url = str_replace(
-                    array(
-                        $wp_site_env['wp_active_theme'],
-                        $wp_site_env['wp_themes'],
-                        $wp_site_env['wp_uploads'],
-                        $wp_site_env['wp_plugins'],
-                        $wp_site_env['wp_content'],
-                        $wp_site_env['wp_inc'],
-                    ),
-                    array(
-                        $new_site_paths['new_active_theme_path'],
-                        $new_site_paths['new_themes_path'],
-                        $new_site_paths['new_uploads_path'],
-                        $new_site_paths['new_plugins_path'],
-                        $new_site_paths['new_wp_content_path'],
-                        $new_site_paths['new_wpinc_path'],
-                    ),
-                    $url_to_change
-                );
+        if ( $this->isInternalLink( $url_to_change ) ) {
+            // rewrite URLs, starting with longest paths down to shortest
+            $rewritten_url = str_replace(
+                array(
+                    $this->wp_site_env['wp_active_theme'],
+                    $this->wp_site_env['wp_themes'],
+                    $this->wp_site_env['wp_uploads'],
+                    $this->wp_site_env['wp_plugins'],
+                    $this->wp_site_env['wp_content'],
+                    $this->wp_site_env['wp_inc'],
+                ),
+                array(
+                    $this->new_site_paths['new_active_theme_path'],
+                    $this->new_site_paths['new_themes_path'],
+                    $this->new_site_paths['new_uploads_path'],
+                    $this->new_site_paths['new_plugins_path'],
+                    $this->new_site_paths['new_wp_content_path'],
+                    $this->new_site_paths['new_wpinc_path'],
+                ),
+                $url_to_change
+            );
 
-                $element->setAttribute( $attribute_to_change, $rewritten_url );
-            }
+            $element->setAttribute( $attribute_to_change, $rewritten_url );
         }
     }
 
     public function getHTML() {
         return $this->xml_doc->saveHtml();
-
     }
 
-    // TODO: damn, this is going to take a while to cleanup...
-    // TODO: some optimization to be gained by doing this in same
-    // loop as rewriteWPPaths loop
-    public function rewriteBaseURL(
-        $element,
-        $old_URL,
-        $new_URL,
-        $allowOfflineUsage,
-        $absolutePaths = false,
-        $useBaseHref = true ) {
+    // NOTE: separate from WP rewrites in case people have disabled that
+    public function rewriteBaseURL( $element ) {
+        if ( $element->hasAttribute( 'href' ) ) {
+            $attribute_to_change = 'href';
+        } elseif ( $element->hasAttribute( 'src' ) ) {
+            $attribute_to_change = 'src';
+            // skip elements without href or src
+        } else {
+            return;
+        }
 
-        // error_log('old base url: ' . $old_URL);
-        // error_log('new base url: ' . $new_URL);
-        foreach ( $this->xml_doc->getElementsByTagName( '*' ) as $element ) {
-            if ( $element->hasAttribute( 'href' ) ) {
-                $attribute_to_change = 'href';
-            } elseif ( $element->hasAttribute( 'src' ) ) {
-                $attribute_to_change = 'src';
-                // skip elements without href or src
-            } else {
-                continue;
-            }
+        $url_to_change = $element->getAttribute( $attribute_to_change );
 
-            $url_to_change = $element->getAttribute( $attribute_to_change );
+        // check it actually needs to be changed
+        // TODO: is this check not done already by isInternalLink()?
+        if ( strpos( $url_to_change, $this->wp_site_url ) !== false ) {
+            $rewritten_url = str_replace(
+                $this->wp_site_url,
+                $this->baseUrl,
+                $url_to_change
+            );
 
-            // check it actually needs to be changed
-            if ( strpos( $url_to_change, $old_URL ) !== false ) {
-                $rewritten_url = str_replace(
-                    $old_URL,
-                    $new_URL,
-                    $url_to_change
-                );
-
-                $element->setAttribute( $attribute_to_change, $rewritten_url );
-            }
+            $element->setAttribute( $attribute_to_change, $rewritten_url );
         }
 
         if ( $absolutePaths ) {
 
             // TODO: re-implement as separate func as another processing layer
             // error_log('SKIPPING absolute path rewriting');
-            if ( $useBaseHref ) {
+            if ( $this->useBaseHref ) {
                 $responseBody = str_replace(
                     '<head>',
                     "<head>\n<base href=\"" .
