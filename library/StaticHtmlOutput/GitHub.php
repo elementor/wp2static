82788edf00d8da68f891ae511840077b67313f2a
Leon Stafford <leon.stafford@mac.com>
2018-09-29 00:05:04 +1000

    linting
    Former-commit-id: 88c31b87fed48a00f24a162e9ded82200568dcd6

--- library/StaticHtmlOutput/GitHub.php
+++ library/StaticHtmlOutput/GitHub.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * @package WP Static HTML Output
- *
- * Copyright (c) 2011 Leon Stafford
- */
 
 use GuzzleHttp\Client;
 
@@ -19,14 +14,19 @@ class StaticHtmlOutput_GitHub {
     protected $_globHashAndPathList;
     protected $_archiveName;
 
-    public function __construct( $userRepository, $accessToken, $branch, $remotePath, $uploadsPath ) {
-        list($this->_user, $this->_repository) = explode( '/', $userRepository );
-        $this->_accessToken = $accessToken;
+    // TODO: args to come from post or settings
+    public function __construct( $repo, $token, $branch, $r_path, $upl_path ) {
+        list($this->_user, $this->_repository) = explode( '/', $repo );
+        $this->_accessToken = $token;
         $this->_branch = $branch;
-        $this->_remotePath = $remotePath;
-        $this->_exportFileList = $uploadsPath . '/WP-STATIC-EXPORT-GITHUB-FILES-TO-EXPORT';
-        $this->_globHashAndPathList = $uploadsPath . '/WP-STATIC-EXPORT-GITHUB-GLOBS-PATHS';
-        $archiveDir = file_get_contents( $uploadsPath . '/WP-STATIC-CURRENT-ARCHIVE' );
+        $this->_remotePath = $r_path;
+        $this->_exportFileList =
+            $upl_path . '/WP-STATIC-EXPORT-GITHUB-FILES-TO-EXPORT';
+        $this->_globHashAndPathList =
+            $upl_path . '/WP-STATIC-EXPORT-GITHUB-GLOBS-PATHS';
+        $archiveDir = file_get_contents(
+            $upl_path . '/WP-STATIC-CURRENT-ARCHIVE'
+        );
         $this->_archiveName = rtrim( $archiveDir, '/' );
     }
 
@@ -44,34 +44,50 @@ class StaticHtmlOutput_GitHub {
         }
     }
 
-    // TODO: move this into a parent class as identical to bunny and probably others
-    public function create_github_deployment_list( $dir, $archiveName, $remotePath ) {
+    // TODO: move into a parent class as identical to bunny and probably others
+    public function create_github_deployment_list( $dir, $archive, $r_path ) {
         $files = scandir( $dir );
 
         foreach ( $files as $item ) {
             if ( $item != '.' && $item != '..' && $item != '.git' ) {
                 if ( is_dir( $dir . '/' . $item ) ) {
-                    $this->create_github_deployment_list( $dir . '/' . $item, $archiveName, $remotePath );
+                    $this->create_github_deployment_list(
+                        $dir . '/' . $item,
+                        $archive,
+                        $r_path
+                    );
                 } elseif ( is_file( $dir . '/' . $item ) ) {
-                    $subdir = str_replace( '/wp-admin/admin-ajax.php', '', $_SERVER['REQUEST_URI'] );
+                    $subdir = str_replace(
+                        '/wp-admin/admin-ajax.php',
+                        '',
+                        $_SERVER['REQUEST_URI']
+                    );
                     $subdir = ltrim( $subdir, '/' );
-                    $clean_dir = str_replace( $archiveName . '/', '', $dir . '/' );
+                    $clean_dir = str_replace( $archive . '/', '', $dir . '/' );
                     $clean_dir = str_replace( $subdir, '', $clean_dir );
-                    $targetPath = $remotePath . $clean_dir;
+                    $targetPath = $r_path . $clean_dir;
                     $targetPath = ltrim( $targetPath, '/' );
-                    $export_line = $dir . '/' . $item . ',' . $targetPath . "\n";
-                    file_put_contents( $this->_exportFileList, $export_line, FILE_APPEND | LOCK_EX );
+                    $export_line =
+                        $dir . '/' . $item . ',' . $targetPath . "\n";
+                    file_put_contents(
+                        $this->_exportFileList,
+                        $export_line,
+                        FILE_APPEND | LOCK_EX
+                    );
                 }
             }
         }
     }
 
-
-    // TODO: move this into a parent class as identical to bunny and probably others
+    // TODO: move to a parent class as identical to bunny and probably others
     public function prepare_deployment() {
 
             $this->clear_file_list();
-            $this->create_github_deployment_list( $this->_archiveName, $this->_archiveName, $this->_remotePath );
+            $this->create_github_deployment_list(
+                $this->_archiveName,
+                $this->_archiveName,
+                $this->_remotePath
+            );
 
             echo 'SUCCESS';
     }
@@ -81,10 +97,12 @@ class StaticHtmlOutput_GitHub {
         $line = fgets( $f );
         fclose( $f );
 
-        // TODO reduce the 2 file reads here, this one is just trimming the first line
         $contents = file( $this->_exportFileList, FILE_IGNORE_NEW_LINES );
         array_shift( $contents );
-        file_put_contents( $this->_exportFileList, implode( "\r\n", $contents ) );
+        file_put_contents(
+            $this->_exportFileList,
+            implode( "\r\n", $contents )
+        );
 
         return $line;
     }
@@ -99,7 +117,8 @@ class StaticHtmlOutput_GitHub {
 
 
     public function upload_blobs( $viaCLI = false ) {
-            require_once dirname( __FILE__ ) . '/../GuzzleHttp/autoloader.php';
+            require_once dirname( __FILE__ ) .
+                '/../GuzzleHttp/autoloader.php';
             require_once __DIR__ . '/../Github/autoload.php';
 
         if ( $this->get_remaining_items_count() < 0 ) {
@@ -114,9 +133,14 @@ class StaticHtmlOutput_GitHub {
             $targetPath = rtrim( $targetPath );
 
             // vendor specific from here
-            $encodedFile = chunk_split( base64_encode( file_get_contents( $fileToTransfer ) ) );
+            $encodedFile = chunk_split(
+                base64_encode( file_get_contents( $fileToTransfer ) )
+            );
             $client = new \Github\Client();
-            $client->authenticate( $this->_accessToken, Github\Client::AUTH_HTTP_TOKEN );
+            $client->authenticate(
+                $this->_accessToken,
+                Github\Client::AUTH_HTTP_TOKEN
+            );
 
         try {
             $globHash = $client->api( 'gitData' )->blobs()->create(
@@ -128,22 +152,26 @@ class StaticHtmlOutput_GitHub {
                 )
             ); // utf-8 or base64
         } catch ( Exception $e ) {
-            WsLog::l( 'GITHUB: Error creating blob (API limits exceeded?):' . $e );
-            error_log( 'error creating blog in GitHub (API limits exceeded?)' );
-            // TODO: show current rate limits: https://developer.github.com/v3/rate_limit/
+            WsLog::l( 'GITHUB: Error creating blob (API limits?):' . $e );
+            error_log( 'error creating blog in GitHub (API limits?)' );
+            // TODO:  rate limits: https://developer.github.com/v3/rate_limit/
             $coreLimit = $client->api( 'rate_limit' )->getCoreLimit();
             error_log( $coreLimit );
         }
 
-            $globHashPathLine = $globHash['sha'] . ',' . rtrim( $targetPath ) . basename( $fileToTransfer ) . "\n";
-            file_put_contents( $this->_globHashAndPathList, $globHashPathLine, FILE_APPEND | LOCK_EX );
+            $globHashPathLine = $globHash['sha'] . ',' .
+                rtrim( $targetPath ) . basename( $fileToTransfer ) . "\n";
 
-            // end vendor specific
+            file_put_contents(
+                $this->_globHashAndPathList,
+                $globHashPathLine,
+                FILE_APPEND | LOCK_EX
+            );
+
             $filesRemaining = $this->get_remaining_items_count();
 
         if ( $this->get_remaining_items_count() > 0 ) {
 
-            // if this is via CLI, then call this function again here
             if ( $viaCLI ) {
                 $this->upload_blobs( true );
             }
@@ -161,9 +189,20 @@ class StaticHtmlOutput_GitHub {
         // vendor specific from here
         $client = new \Github\Client();
 
-        $client->authenticate( $this->_accessToken, Github\Client::AUTH_HTTP_TOKEN );
-        $reference = $client->api( 'gitData' )->references()->show( $this->_user, $this->_repository, 'heads/' . $this->_branch );
-        $commit = $client->api( 'gitData' )->commits()->show( $this->_user, $this->_repository, $reference['object']['sha'] );
+        $client->authenticate(
+            $this->_accessToken,
+            Github\Client::AUTH_HTTP_TOKEN
+        );
+        $reference = $client->api( 'gitData' )->references()->show(
+            $this->_user,
+            $this->_repository,
+            'heads/' . $this->_branch
+        );
+        $commit = $client->api( 'gitData' )->commits()->show(
+            $this->_user,
+            $this->_repository,
+            $reference['object']['sha']
+        );
         $commitSHA = $commit['sha'];
         $treeSHA = $commit['tree']['sha'];
         $treeURL = $commit['tree']['url'];
@@ -186,14 +225,23 @@ class StaticHtmlOutput_GitHub {
             'tree' => $treeContents,
         );
 
-        $newTree = $client->api( 'gitData' )->trees()->create( $this->_user, $this->_repository, $treeData );
+        $newTree = $client->api( 'gitData' )->trees()->create(
+            $this->_user,
+            $this->_repository,
+            $treeData
+        );
 
         $commitData = array(
-            'message' => 'WP Static HTML Output plugin: ' . date( 'Y-m-d h:i:s' ),
+            'message' =>
+                'WP Static HTML Output plugin: ' . date( 'Y-m-d h:i:s' ),
             'tree' => $newTree['sha'],
             'parents' => array( $commitSHA ),
         );
-        $commit = $client->api( 'gitData' )->commits()->create( $this->_user, $this->_repository, $commitData );
+        $commit = $client->api( 'gitData' )->commits()->create(
+            $this->_user,
+            $this->_repository,
+            $commitData
+        );
         $referenceData = array(
             'sha' => $commit['sha'],
             'force' => true,
