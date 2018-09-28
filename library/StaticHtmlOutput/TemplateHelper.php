0201b7d0714f67192afcc15817eb5fa3aed4ef36
Leon Stafford <leon.stafford@mac.com>
2018-09-29 00:38:30 +1000

    disable linting template helper
    Former-commit-id: d30abacbb0deb55d3a75bff24f604cab339a8d33

--- library/StaticHtmlOutput/TemplateHelper.php
+++ library/StaticHtmlOutput/TemplateHelper.php
@@ -1,38 +1,38 @@
 <?php
 
+// phpcs:disable
 class TemplateHelper {
     public function __construct() {
 
     }
 
-    public function displayCheckbox( $template_variables, $field_name, $field_label ) {
-
+    public function displayCheckbox( $tpl_vars, $field_name, $field_label ) {
         echo "
       <fieldset>
         <label for='{$field_name}'>
-          <input name='{$field_name}' id='{$field_name}' value='1' type='checkbox' " . ( $template_variables->options->{$field_name} === '1' ? 'checked' : '' ) . ' />
+          <input name='{$field_name}' id='{$field_name}' value='1' type='checkbox' " . ( $tpl_vars->options->{$field_name} === '1' ? 'checked' : '' ) . ' />
           <span>' . __( $field_label, 'static-html-output-plugin' ) . '</span>
         </label>
       </fieldset>
     ';
     }
 
-    public function displayTextfield( $template_variables, $field_name, $field_label, $description, $type = 'text' ) {
+    public function displayTextfield( $tpl_vars, $field_name, $field_label, $description, $type = 'text' ) {
         echo "
-      <input name='{$field_name}' class='regular-text' id='{$field_name}' type='{$type}' value='" . esc_attr( $template_variables->options->{$field_name} ) . "' placeholder='" . __( $field_label, 'static-html-output-plugin' ) . "' />
+      <input name='{$field_name}' class='regular-text' id='{$field_name}' type='{$type}' value='" . esc_attr( $tpl_vars->options->{$field_name} ) . "' placeholder='" . __( $field_label, 'static-html-output-plugin' ) . "' />
       <span class='description'>$description</span>
       <br>
     ";
     }
 
-    public function displaySelectMenu( $template_variables, $menu_options, $field_name, $field_label, $description, $type = 'text' ) {
+    public function displaySelectMenu( $tpl_vars, $menu_options, $field_name, $field_label, $description, $type = 'text' ) {
 
         $menu_code = "
       <select name='{$field_name}' id='{$field_name}'>
         <option></option>";
 
         foreach ( $menu_options as $value => $text ) {
-            if ( $template_variables->options->{$field_name} === $value ) {
+            if ( $tpl_vars->options->{$field_name} === $value ) {
                 $menu_code .= "
             <option value='{$value}' selected>{$text}</option>";
             } else {
@@ -52,3 +52,4 @@ class TemplateHelper {
         echo $value;
     }
 }
+// phpcs:enable
