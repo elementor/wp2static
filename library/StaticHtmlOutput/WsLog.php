0795577577c3a076ccd4d76597306cb13569ba01
Leon Stafford <leon.stafford@mac.com>
2018-09-29 00:49:06 +1000

    linting

--- library/StaticHtmlOutput/WsLog.php
+++ library/StaticHtmlOutput/WsLog.php
@@ -4,11 +4,13 @@ class WsLog {
 
     public static function l( $text ) {
         $wp_uploads_path = $_POST['wp_uploads_path'];
-        $working_directory = isset( $_POST['workingDirectory'] ) ? $_POST['workingDirectory'] : $this->wp_uploads_path;
+        $working_directory = isset( $_POST['workingDirectory'] ) ?
+            $_POST['workingDirectory'] : $this->wp_uploads_path;
 
         $log_file_path = $working_directory . '/WP-STATIC-EXPORT-LOG';
 
-        $myfile = fopen( $log_file_path, 'a' ) or die( 'Unable to open file!' );
+        $myfile =
+            fopen( $log_file_path, 'a' ) || die( 'Unable to open file!' );
         fwrite( $myfile, $text . PHP_EOL );
         fclose( $myfile );
     }
