9da1775ca873433a44173710d38bc6f29add658d
Leon Stafford <leon.stafford@mac.com>
2018-10-06 00:48:11 +1000

    expand data provider in URL normalization test
    Former-commit-id: c159b1257ecb35a9df22d7242d68f2b3c7253bf0

--- provisioning/tests/HTMLProcessor/normalizeURLTest.php
+++ provisioning/tests/HTMLProcessor/normalizeURLTest.php
@@ -9,47 +9,77 @@ use PHPUnit\Framework\TestCase;
 
 final class HTMLProcessorNormalizeURLTest extends TestCase {
 
-    public function testNormalizePartialURLInAnchor(): void {
+    /**
+     * @dataProvider anchorTagProvider
+     */
+    public function testNormalizePartialURLInAnchor(
+        $node_html,
+        $tag_name,
+        $attr,
+        $exp_result
+        ) {
         $html_doc = new DOMDocument();
-
-        $html_string = <<<EOHTML
-<!DOCTYPE html>
-<html lang="en-US" class="no-js no-svg">
-<body>
-
-<a href="/first_lvl_dir/a_file.jpg">Link to some file</a>
-
-</body>
-</html>
-EOHTML;
-
-        $html_doc->loadHTML( $html_string );
-        $links = $html_doc->getElementsByTagName( 'a' );
+        $html_header = '<!DOCTYPE html><html lang="en-US" class="no-js no-svg"><body>';
+        $html_footer = '</body></html>';
+        $html_doc->loadHTML( $html_header . $node_html . $html_footer);
+        $links = $html_doc->getElementsByTagName( $tag_name );
         $element = $links[0];
-        $attribute = 'href';
 
         // mock out only the unrelated methods
-        $mockUrlResponse = $this->getMockBuilder( 'HTMLProcessor' )
+        $mockProcessor = $this->getMockBuilder( 'HTMLProcessor' )
             ->setMethods(
                 [
                     'isInternalLink',
-                    'getResponseBody',
-                    'setResponseBody',
                 ]
             )
             ->getMock();
 
-        $mockUrlResponse->page_url = new Net_URL2(
+        $mockProcessor->page_url = new Net_URL2(
             'http://mywpsite.com/category/photos/my-gallery/'
         );
 
-        $mockUrlResponse->method( 'isInternalLink' )->willReturn( true );
-        $mockUrlResponse->expects( $this->once() )->method( 'isInternalLink' );
-        $mockUrlResponse->normalizeURL( $element, $attribute );
+        $mockProcessor->method( 'isInternalLink' )->willReturn( true );
+        $mockProcessor->expects( $this->once() )->method( 'isInternalLink' );
+        $mockProcessor->normalizeURL( $element, $attr );
 
         $this->assertEquals(
-            $element->getAttribute( 'href' ),
-            'http://mywpsite.com/first_lvl_dir/a_file.jpg'
+            $exp_result,
+            $element->ownerDocument->saveHTML( $element )
         );
+    }
+
+    public function anchorTagProvider() {
+        return [
+           'anchor tag with relative href' =>  [
+                '<a href="/first_lvl_dir/a_file.jpg">Link to some file</a>',
+                'a',
+                'href',
+                '<a href="http://mywpsite.com/first_lvl_dir/a_file.jpg">Link to some file</a>'
+            ],
+           'img tag with relative src' =>  [
+                '<img src="/first_lvl_dir/a_file.jpg" />',
+                'img',
+                'src',
+                '<img src="http://mywpsite.com/first_lvl_dir/a_file.jpg">'
+            ],
+           'script tag with relative src and malformed tag' =>  [
+                '<script src="/some.js" />',
+                'script',
+                'src',
+                '<script src="http://mywpsite.com/some.js"></script>'
+            ],
+           'link tag with href to file at same hierachy' =>  [
+                '<link rel="stylesheet" type="text/css" href="theme.css">',
+                'link',
+                'href',
+                '<link rel="stylesheet" type="text/css" href="http://mywpsite.com/category/photos/my-gallery/theme.css">'
+            ],
+           'link tag with href to site root' =>  [
+                '<link rel="stylesheet" type="text/css" href="/">',
+                'link',
+                'href',
+                '<link rel="stylesheet" type="text/css" href="http://mywpsite.com/">'
+            ],
+        ];
     }
 }
