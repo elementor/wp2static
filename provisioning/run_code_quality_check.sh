6dfbe986a4561303ab9e0672242e7ab759c0a72f
Leon Stafford <lionhive@gmail.com>
2018-10-19 11:09:52 +1000

    run code check from CLI
    Former-commit-id: 445f55fa0cae0cef60386a53c05633a30ad5ff01

---
+++ provisioning/run_code_quality_check.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+$HOME/PHP_CodeSniffer/bin/phpcs --ignore=*/provisioning/*,*/freemius/*,*/GuzzleHttp/*,*/Github/*,*/aws/*,*/CloudFront/*,*/Psr/*,*/CSSParser/*,*/FTP/*,*/S3/*,*/URL2/*,wp-static-html-output.php ./
