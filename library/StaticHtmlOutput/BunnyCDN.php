91b98f00ecf563df61045d951292d016a408c24d
Leon Stafford <leon@wp2static.com>
2018-11-16 18:26:05 +0700

    deployer superclass;optimize BunnyCDN deploys

--- library/StaticHtmlOutput/BunnyCDN.php
+++ library/StaticHtmlOutput/BunnyCDN.php
@@ -2,7 +2,7 @@
 
 use GuzzleHttp\Client;
 
-class StaticHtmlOutput_BunnyCDN {
+class StaticHtmlOutput_BunnyCDN extends StaticHtmlOutput_SitePublisher{
 
     public function __construct() {
         $target_settings = array(
@@ -30,6 +30,11 @@ class StaticHtmlOutput_BunnyCDN {
                 '/WP-STATIC-CURRENT-ARCHIVE'
         );
 
+        $this->r_path = '';
+        if ( isset( $this->settings['bunnycdnRemotePath'] ) ) {
+            $this->r_path = $this->settings['bunnycdnRemotePath'];
+        }
+
         // TODO: move this where needed
         require_once dirname( __FILE__ ) .
             '/../StaticHtmlOutput/Archive.php';
@@ -52,126 +57,6 @@ class StaticHtmlOutput_BunnyCDN {
         }
     }
 
-    public function clear_file_list() {
-        if ( is_file( $this->exportFileList ) ) {
-            $f = fopen( $this->exportFileList, 'r+' );
-            if ( $f !== false ) {
-                ftruncate( $f, 0 );
-                fclose( $f );
-            }
-        }
-    }
-
-    public function create_bunny_deployment_list( $dir ) {
-        $r_path = '';
-        $archive = $this->archive->path;
-
-        if ( isset( $this->settings['bunnycdnRemotePath'] ) ) {
-            $r_path = $this->settings['bunnycdnRemotePath'];
-        }
-
-        $files = scandir( $dir );
-
-        foreach ( $files as $item ) {
-            if ( $item != '.' && $item != '..' && $item != '.git' ) {
-                if ( is_dir( $dir . '/' . $item ) ) {
-                    $this->create_bunny_deployment_list( $dir . '/' . $item );
-                } elseif ( is_file( $dir . '/' . $item ) ) {
-                    $wp_subdir = str_replace(
-                        '/wp-admin/admin-ajax.php',
-                        '',
-                        $_SERVER['REQUEST_URI']
-                    );
-
-                    $wp_subdir = ltrim( $subdir, '/' );
-                    $dirs_in_path = $dir;
-                    $filename = $item;
-                    $original_filepath = $dir . '/' . $item;
-
-                    $local_path_to_strip = $archive . '/' . $wp_subdir;
-                    $local_path_to_strip = rtrim( $local_path_to_strip, '/' );
-
-                    $deploy_path = str_replace(
-                        $local_path_to_strip,
-                        '',
-                        $dirs_in_path
-                    );
-
-                    $original_file_without_archive = str_replace(
-                        $local_path_to_strip,
-                        '',
-                        $original_filepath
-                    );
-
-                    $original_file_without_archive = ltrim(
-                        $original_file_without_archive,
-                        '/'
-                    );
-
-                    $deploy_path = $r_path . $deploy_path;
-                    $deploy_path = ltrim( $deploy_path, '/' );
-                    $deploy_path .= '/';
-
-                    $export_line =
-                        $original_file_without_archive . ',' . // field 1
-                        $deploy_path . // field 2
-                        "\n";
-
-                    file_put_contents(
-                        $this->exportFileList,
-                        $export_line,
-                        FILE_APPEND | LOCK_EX
-                    );
-                }
-            }
-        }
-    }
-
-    public function prepare_export() {
-        $this->clear_file_list();
-
-        $this->create_bunny_deployment_list(
-            $this->settings['working_directory'] . '/' .
-                $this->archive->name
-        );
-
-        echo 'SUCCESS';
-    }
-
-    public function get_items_to_export( $batch_size = 1 ) {
-        $lines = array();
-
-        $f = fopen( $this->exportFileList, 'r' );
-
-        for ( $i = 0; $i < $batch_size; $i++ ) {
-            $lines[] = fgets( $f );
-        }
-
-        fclose( $f );
-
-        // TODO: optimize this for just one read, one write within func
-        $contents = file( $this->exportFileList, FILE_IGNORE_NEW_LINES );
-
-        for ( $i = 0; $i < $batch_size; $i++ ) {
-            // rewrite file minus the lines we took
-            array_shift( $contents );
-        }
-
-        file_put_contents(
-            $this->exportFileList,
-            implode( "\r\n", $contents )
-        );
-
-        return $lines;
-    }
-
-    public function get_remaining_items_count() {
-        $contents = file( $this->exportFileList, FILE_IGNORE_NEW_LINES );
-
-        // return the amount left if another item is taken
-        // return count($contents) - 1;
-        return count( $contents );
-    }
 
     public function transfer_files( $viaCLI = false ) {
         require_once dirname( __FILE__ ) . '/../GuzzleHttp/autoloader.php';
@@ -191,19 +76,18 @@ class StaticHtmlOutput_BunnyCDN {
 
         $lines = $this->get_items_to_export( $batch_size );
 
-        foreach ( $lines as $line ) {
+        $client = new Client(
+            array(
+                'base_uri' => 'https://storage.bunnycdn.com',
+            )
+        );
 
+        foreach ( $lines as $line ) {
             list($fileToTransfer, $targetPath) = explode( ',', $line );
 
             $fileToTransfer = $this->archive->path . $fileToTransfer;
 
             $targetPath = rtrim( $targetPath );
-
-            $client = new Client(
-                array(
-                    'base_uri' => 'https://storage.bunnycdn.com',
-                )
-            );
 
             try {
                 $target_path = '/' . $this->settings['bunnycdnPullZoneName'] .
---
+++ library/StaticHtmlOutput/SitePublisher.php
@@ -0,0 +1,126 @@
+<?php
+
+use GuzzleHttp\Client;
+
+class StaticHtmlOutput_SitePublisher {
+
+    public function __construct() {
+    }
+
+    public function clear_file_list() {
+        if ( is_file( $this->exportFileList ) ) {
+            $f = fopen( $this->exportFileList, 'r+' );
+            if ( $f !== false ) {
+                ftruncate( $f, 0 );
+                fclose( $f );
+            }
+        }
+    }
+
+    public function create_deployment_list( $dir ) {
+        $archive = $this->archive->path;
+
+        $files = scandir( $dir );
+
+        foreach ( $files as $item ) {
+            if ( $item != '.' && $item != '..' && $item != '.git' ) {
+                if ( is_dir( $dir . '/' . $item ) ) {
+                    $this->create_deployment_list( $dir . '/' . $item );
+                } elseif ( is_file( $dir . '/' . $item ) ) {
+                    $wp_subdir = str_replace(
+                        '/wp-admin/admin-ajax.php',
+                        '',
+                        $_SERVER['REQUEST_URI']
+                    );
+
+                    $wp_subdir = ltrim( $subdir, '/' );
+                    $dirs_in_path = $dir;
+                    $filename = $item;
+                    $original_filepath = $dir . '/' . $item;
+
+                    $local_path_to_strip = $archive . '/' . $wp_subdir;
+                    $local_path_to_strip = rtrim( $local_path_to_strip, '/' );
+
+                    $deploy_path = str_replace(
+                        $local_path_to_strip,
+                        '',
+                        $dirs_in_path
+                    );
+
+                    $original_file_without_archive = str_replace(
+                        $local_path_to_strip,
+                        '',
+                        $original_filepath
+                    );
+
+                    $original_file_without_archive = ltrim(
+                        $original_file_without_archive,
+                        '/'
+                    );
+
+                    $deploy_path = $this->r_path . $deploy_path;
+                    $deploy_path = ltrim( $deploy_path, '/' );
+                    $deploy_path .= '/';
+
+                    $export_line =
+                        $original_file_without_archive . ',' . // field 1
+                        $deploy_path . // field 2
+                        "\n";
+
+                    file_put_contents(
+                        $this->exportFileList,
+                        $export_line,
+                        FILE_APPEND | LOCK_EX
+                    );
+                }
+            }
+        }
+    }
+
+    public function prepare_export() {
+        $this->clear_file_list();
+
+        $this->create_deployment_list(
+            $this->settings['working_directory'] . '/' .
+                $this->archive->name
+        );
+
+        echo 'SUCCESS';
+    }
+
+    public function get_items_to_export( $batch_size = 1 ) {
+        $lines = array();
+
+        $f = fopen( $this->exportFileList, 'r' );
+
+        for ( $i = 0; $i < $batch_size; $i++ ) {
+            $lines[] = fgets( $f );
+        }
+
+        fclose( $f );
+
+        // TODO: optimize this for just one read, one write within func
+        $contents = file( $this->exportFileList, FILE_IGNORE_NEW_LINES );
+
+        for ( $i = 0; $i < $batch_size; $i++ ) {
+            // rewrite file minus the lines we took
+            array_shift( $contents );
+        }
+
+        file_put_contents(
+            $this->exportFileList,
+            implode( "\r\n", $contents )
+        );
+
+        return $lines;
+    }
+
+    public function get_remaining_items_count() {
+        $contents = file( $this->exportFileList, FILE_IGNORE_NEW_LINES );
+
+        // return the amount left if another item is taken
+        // return count($contents) - 1;
+        return count( $contents );
+    }
+}
+
