78062c0bdb74b016be9c708be9cb139a6347ea1c
Leon <leon@wp2static.com>
2018-12-03 22:43:43 +0800

    adjust powerpack build;ignore syml

--- provisioning/powerpack_release.sh
+++ provisioning/powerpack_release.sh
@@ -10,16 +10,21 @@ mkdir -p $TMP_DIR
 rm -Rf $TMP_DIR/wordpress-static-html-plugin
 mkdir $TMP_DIR/wordpress-static-html-plugin
 
-cp -r ./{css,languages,library,readme.txt,views,wp2static.php} $TMP_DIR/wordpress-static-html-plugin/
+cp -r ./{languages,library,readme.txt,views,wp2static.php} $TMP_DIR/wordpress-static-html-plugin/
 
-cp -r ./powerpack $TMP_DIR/wordpress-static-html-plugin/
+mkdir -p $TMP_DIR/wordpress-static-html-plugin/powerpack
+cp -r ./provisioning/deployment_modules/* $TMP_DIR/wordpress-static-html-plugin/powerpack/
 
 # TODO: keep size smaller, strip comments any other dev bloat
 
 cd $TMP_DIR
 
+# tidy permissions
+find . -type d -exec chmod 755 {} \;
+find . -type f -exec chmod 644 {} \;
+
 zip -r -9 ./$1_with_powerpack.zip ./wordpress-static-html-plugin
 
 cd -
 
-cp $TMP_DIR/$1_with_powerpack.zip ../
+cp $TMP_DIR/$1_with_powerpack.zip $HOME/Downloads/
