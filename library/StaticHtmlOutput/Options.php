2749c28b6e35e05495c4ade5e62d031b478d8f49
Leon <leon@wp2static.com>
2018-12-06 01:04:11 +0800

    Base URL -> Destination URL;wip WP-CLI refactor
    Former-commit-id: c2923f5b93c34dee72fa572062b07c3b2a074da7

--- library/StaticHtmlOutput/Options.php
+++ library/StaticHtmlOutput/Options.php
@@ -3,48 +3,7 @@
 class StaticHtmlOutput_Options {
     protected $_options = array();
     protected $_optionKey = null;
-
-    public function __construct( $optionKey ) {
-        $options = get_option( $optionKey );
-
-        if ( false === $options ) {
-            $options = array();
-        }
-
-        $this->_options = $options;
-        $this->_optionKey = $optionKey;
-    }
-
-    public function __set( $name, $value ) {
-        $this->_options[ $name ] = $value;
-
-        return $this;
-    }
-
-    public function setOption( $name, $value ) {
-        return $this->__set( $name, $value );
-    }
-
-    public function __get( $name ) {
-        $value = array_key_exists( $name, $this->_options ) ?
-            $this->_options[ $name ] : null;
-        return $value;
-    }
-
-    public function getOption( $name ) {
-        return $this->__get( $name );
-    }
-
-    public function save() {
-        return update_option( $this->_optionKey, $this->_options );
-    }
-
-    public function delete() {
-        return delete_option( $this->_optionKey );
-    }
-
-    public function saveAllPostData() {
-        $options_to_save = array(
+    protected $_options_to_save = array(
             'additionalUrls',
             'allowOfflineUsage',
             'baseUrl',
@@ -127,7 +86,51 @@ class StaticHtmlOutput_Options {
             'workingDirectory',
         );
 
-        foreach ( $options_to_save as $option ) {
+    public function __construct( $optionKey ) {
+        $options = get_option( $optionKey );
+
+        if ( false === $options ) {
+            $options = array();
+        }
+
+        $this->_options = $options;
+        $this->_optionKey = $optionKey;
+    }
+
+    public function __set( $name, $value ) {
+        $this->_options[ $name ] = $value;
+
+        return $this;
+    }
+
+    public function setOption( $name, $value ) {
+        return $this->__set( $name, $value );
+    }
+
+    public function __get( $name ) {
+        $value = array_key_exists( $name, $this->_options ) ?
+            $this->_options[ $name ] : null;
+        return $value;
+    }
+
+    public function getOption( $name ) {
+        return $this->__get( $name );
+    }
+
+    public function optionExists( $name ) {
+        return in_array( $name, $this->_options_to_save );
+    }
+
+    public function save() {
+        return update_option( $this->_optionKey, $this->_options );
+    }
+
+    public function delete() {
+        return delete_option( $this->_optionKey );
+    }
+
+    public function saveAllPostData() {
+        foreach ( $this->_options_to_save as $option ) {
             // TODO: set which fields should get which sanitzation upon saving
             // TODO: validate before save & avoid making empty settings fields
             $this->setOption( $option, filter_input( INPUT_POST, $option ) );
