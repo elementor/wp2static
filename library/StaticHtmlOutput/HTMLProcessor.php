360ab56d045ad31e5e2c9f47f4db182270974943
Leon <leon@wp2static.com>
2018-12-06 11:34:58 +0800

    record 404s;fix crawl issue

--- library/StaticHtmlOutput/HTMLProcessor.php
+++ library/StaticHtmlOutput/HTMLProcessor.php
@@ -312,12 +312,19 @@ class HTMLProcessor {
     }
 
     public function writeDiscoveredURLs() {
-        if ( $_POST['ajax_action'] === 'crawl_site' ) {
+        if ( $_POST['ajax_action'] === 'crawl_again' ) {
             return;
         }
 
+        if ( defined( 'WP_CLI' ) ) {
+            if ( defined( 'CRAWLING_DISCOVERED' ) ) {
+               return; 
+            }    
+        }
+
         file_put_contents(
-            $this->settings['wp_uploads_path'] . '/WP-STATIC-DISCOVERED-URLS.txt',
+            $this->settings['wp_uploads_path'] .
+                '/WP-STATIC-DISCOVERED-URLS.txt',
             PHP_EOL .
                 implode( PHP_EOL, array_unique( $this->discovered_urls ) ),
             FILE_APPEND | LOCK_EX
