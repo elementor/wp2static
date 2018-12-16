ac37a232810cc12be456b1f4f3c9fa13536fb36d
Leon Stafford <lionhive@gmail.com>
2018-12-16 16:15:51 +0700

    adjust provisioning scripts
    Former-commit-id: 0fa4dc1c15451c67db7b2b3612b920de53cd2f9f

--- provisioning/powerpack_release.sh
+++ provisioning/powerpack_release.sh
@@ -1,6 +1,7 @@
 #!/bin/ksh
 
 # run from project root
+EXEC_DIR=$(pwd)
 
 # give argument for name of zip to be created, ie user-leon-test-new-function
 
@@ -10,10 +11,14 @@ mkdir -p $TMP_DIR
 rm -Rf $TMP_DIR/wordpress-static-html-plugin
 mkdir $TMP_DIR/wordpress-static-html-plugin
 
-cp -r ./{languages,library,readme.txt,views,wp2static.php} $TMP_DIR/wordpress-static-html-plugin/
+cp -r $EXEC_DIR/languages $TMP_DIR/wordpress-static-html-plugin/
+cp -r $EXEC_DIR/library $TMP_DIR/wordpress-static-html-plugin/
+cp -r $EXEC_DIR/readme.txt $TMP_DIR/wordpress-static-html-plugin/
+cp -r $EXEC_DIR/views $TMP_DIR/wordpress-static-html-plugin/
+cp -r $EXEC_DIR/wp2static.php $TMP_DIR/wordpress-static-html-plugin/
 
 mkdir -p $TMP_DIR/wordpress-static-html-plugin/powerpack
-cp -r ./provisioning/deployment_modules/* $TMP_DIR/wordpress-static-html-plugin/powerpack/
+cp -r $EXEC_DIR/provisioning/deployment_modules/* $TMP_DIR/wordpress-static-html-plugin/powerpack/
 
 # TODO: keep size smaller, strip comments any other dev bloat
 
