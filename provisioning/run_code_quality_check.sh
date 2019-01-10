6648b7996d0dffc09bc03b38ffbfbf385cb44767
Leon Stafford <lionhive@gmail.com>
2019-01-10 15:35:34 +0700

    ignore untracked files in code sniffs
    Former-commit-id: f46b9caff35a2781905769813478cf1f2a7fdb22

--- provisioning/run_code_quality_check.sh
+++ provisioning/run_code_quality_check.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$HOME/PHP_CodeSniffer/bin/phpcs --ignore=*/provisioning/*,*/GuzzleHttp/*,*/Github/*,*/aws/*,*/CloudFront/*,*/Psr/*,*/CSSParser/*,*/FTP/*,*/S3/*,*/URL2/*,wp2static.php ./
+$HOME/PHP_CodeSniffer/bin/phpcs --ignore=*/powerpack/*,*/provisioning/*,*/GuzzleHttp/*,*/Github/*,*/aws/*,*/CloudFront/*,*/Psr/*,*/CSSParser/*,*/FTP/*,*/S3/*,*/URL2/*,wp2static.php ./
