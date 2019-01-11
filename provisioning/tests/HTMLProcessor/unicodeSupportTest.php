08250000e2ad7d8b654d43830d0d66bbd18a6efe
Leon Stafford <lionhive@gmail.com>
2019-01-11 15:48:45 +0700

    failing test for #220 - unicode support
    Former-commit-id: e91e6d002a6c4fa6c95de5d0328bf06af268fe93

---
+++ provisioning/tests/HTMLProcessor/unicodeSupportTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+require_once 'library/StaticHtmlOutput/HTMLProcessor.php';
+require_once 'library/URL2/URL2.php';
+
+use PHPUnit\Framework\TestCase;
+
+final class HTMLProcessorUnicodeSupportTest extends TestCase {
+
+    /**
+     * @dataProvider unicodeProvider
+     */
+    public function testUnicodeOutput(
+        $test_HTML_content,
+        $exp_result
+        ) {
+
+        $mockProcessor = $this->getMockBuilder( 'HTMLProcessor' )
+            ->setMethods(
+                [
+                    'loadSettings',
+                    'rewriteSiteURLsToPlaceholder',
+                    'detectIfURLsShouldBeHarvested',
+                    'writeDiscoveredURLs',
+                ]
+            )
+            ->getMock();
+
+        $page_URL = new Net_URL2(
+            'http://mywpsite.com/category/photos/my-gallery/'
+        );
+
+        $mockProcessor->method( 'loadSettings' )->willReturn( null );
+        $mockProcessor->method( 'rewriteSiteURLsToPlaceholder' )->willReturn( null );
+        $mockProcessor->method( 'detectIfURLsShouldBeHarvested' )->willReturn( null );
+        $mockProcessor->method( 'writeDiscoveredURLs' )->willReturn( null );
+
+        $mockProcessor->settings = array(
+            'baseUrl' => 'http://baseurldomainfromsettings.com/',
+        );
+
+        $mockProcessor->processHTML( $test_HTML_content, $page_URL );
+
+        $this->assertEquals(
+            $exp_result,
+            $mockProcessor->xml_doc->saveHTML()
+        );
+
+    }
+
+    public function unicodeProvider() {
+        return [
+           'unicode characters in source' =>  [
+                '<!DOCTYPE html><html lang="en-US"><head></head><title>wpnotes | Поредният WordPress сайт</title><body></body></html>',
+                '<!DOCTYPE html>
+<html lang="en-US"><head></head><title>wpnotes | Поредният WordPress сайт</title><body></body></html>
+',
+            ],
+        ];
+    }
+}
