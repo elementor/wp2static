a400b620fa605e57a1915f6c380186fb52672c13
Leon Stafford <lionhive@gmail.com>
2019-01-14 04:25:10 +0700

    Netlify using cURL

--- provisioning/deployment_modules/Netlify.php
+++ provisioning/deployment_modules/Netlify.php
@@ -1,7 +1,5 @@
 <?php
 
-use GuzzleHttp\Client;
-
 class StaticHtmlOutput_Netlify {
 
     public function __construct() {
@@ -22,12 +20,11 @@ class StaticHtmlOutput_Netlify {
                 '/../library/StaticHtmlOutput/DBSettings.php';
 
             $this->settings = WPSHO_DBSettings::get( $target_settings );
-
         }
 
         $this->settings['netlifySiteID'];
         $this->settings['netlifyPersonalAccessToken'];
-        $this->baseURL = 'https://api.netlify.com';
+        $this->base_url = 'https://api.netlify.com';
 
         $this->detectSiteID();
 
@@ -42,16 +39,16 @@ class StaticHtmlOutput_Netlify {
     }
 
     public function detectSiteID() {
-        $this->siteID = $this->settings['netlifySiteID'];
+        $this->site_id = $this->settings['netlifySiteID'];
 
-        if ( strpos( $this->siteID, 'netlify.com' ) !== false ) {
+        if ( strpos( $this->site_id, 'netlify.com' ) !== false ) {
             return;
-        } elseif ( strpos( $this->siteID, '.' ) !== false ) {
+        } elseif ( strpos( $this->site_id, '.' ) !== false ) {
             return;
-        } elseif ( strlen( $this->siteID ) === 37 ) {
+        } elseif ( strlen( $this->site_id ) === 37 ) {
             return;
         } else {
-            $this->siteID .= '.netlify.com';
+            $this->site_id .= '.netlify.com';
         }
     }
 
@@ -60,31 +57,55 @@ class StaticHtmlOutput_Netlify {
             '/../library/StaticHtmlOutput/Archive.php';
         $archive = new Archive();
         $archive->setToCurrentArchive();
-        $zipArchivePath = $this->settings['wp_uploads_path'] . '/' .
+        $zip_archive_path = $this->settings['wp_uploads_path'] . '/' .
             $archive->name . '.zip';
 
-        require_once dirname( __FILE__ ) .
-            '/../library/GuzzleHttp/autoloader.php';
-        $client = new Client( array( 'base_uri' => $this->baseURL ) );
-
-        $zipDeployEndpoint =
-            '/api/v1/sites/' .
-            $this->siteID .
-            '/deploys';
+        $zip_deploy_endpoint = $this->base_url . '/api/v1/sites/' .
+            $this->site_id . '/deploys';
 
         try {
-            $response = $client->request(
-                'POST',
-                $zipDeployEndpoint,
+            $ch = curl_init();
+
+            $file_stream = fopen( $zip_archive_path, 'r' );
+            $data_length = filesize( $zip_archive_path );
+
+            curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, 'POST' );
+            curl_setopt( $ch, CURLOPT_URL, $zip_deploy_endpoint );
+            curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+            curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0 );
+            curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0 );
+            curl_setopt( $ch, CURLOPT_HEADER, 0 );
+            curl_setopt( $ch, CURLOPT_UPLOAD, 1 );
+            curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1 );
+            curl_setopt( $ch, CURLOPT_INFILE, $file_stream );
+            curl_setopt( $ch, CURLOPT_INFILESIZE, $data_length );
+
+            curl_setopt(
+                $ch,
+                CURLOPT_HTTPHEADER,
                 array(
-                    'headers'  => array(
-                        'Content-Type' => 'application/zip',
-                        'Authorization' => 'Bearer ' .
-                            $this->settings['netlifyPersonalAccessToken'],
-                    ),
-                    'body' => fopen( $zipArchivePath, 'rb' ),
+                    'Authorization: Bearer ' .
+                        $this->settings['netlifyPersonalAccessToken'],
+                    'Content-Type: application/zip',
                 )
             );
+
+            $output = curl_exec( $ch );
+            $status_code = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
+
+            curl_close( $ch );
+
+            $good_response_codes = array( '200', '201', '301', '302', '304' );
+
+            if ( ! in_array( $status_code, $good_response_codes ) ) {
+                require_once dirname( __FILE__ ) .
+                    '/../library/StaticHtmlOutput/WsLog.php';
+                WsLog::l(
+                    'BAD RESPONSE STATUS (' . $status_code . '): '
+                );
+
+                throw new Exception( 'Netlify API bad response status' );
+            }
 
             if ( ! defined( 'WP_CLI' ) ) {
                 echo 'SUCCESS';
@@ -94,35 +115,54 @@ class StaticHtmlOutput_Netlify {
                 '/../library/StaticHtmlOutput/WsLog.php';
             WsLog::l( 'NETLIFY EXPORT ERROR' );
             WsLog::l( $e );
-            error_log( $e );
             throw new Exception( $e );
         }
     }
 
     public function test_netlify() {
-        require_once dirname( __FILE__ ) .
-            '/../library/GuzzleHttp/autoloader.php';
-
-        $client = new Client( array( 'base_uri' => $this->baseURL ) );
-
-        $site_info_endpoint =
-            '/api/v1/sites/' .
-            $this->siteID;
+        $site_info_endpoint = $this->base_url . '/api/v1/sites/' .
+            $this->site_id;
 
         try {
-            $response = $client->request(
-                'GET',
-                $site_info_endpoint,
+            $ch = curl_init();
+
+            curl_setopt( $ch, CURLOPT_URL, $site_info_endpoint );
+            curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+            curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0 );
+            curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0 );
+            curl_setopt( $ch, CURLOPT_HEADER, 0 );
+            curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1 );
+            curl_setopt( $ch, CURLOPT_USERAGENT, 'WP2Static.com' );
+            curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, 'GET' );
+
+            curl_setopt(
+                $ch,
+                CURLOPT_HTTPHEADER,
                 array(
-                    'headers'  => array(
-                        'Authorization' => 'Bearer ' .
-                            $this->settings['netlifyPersonalAccessToken'],
-                    ),
+                    'Authorization: Bearer ' .
+                        $this->settings['netlifyPersonalAccessToken'],
                 )
             );
 
+            $output = curl_exec( $ch );
+            $status_code = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
+
+            curl_close( $ch );
+
+            $good_response_codes = array( '200', '201', '301', '302', '304' );
+
+            if ( ! in_array( $status_code, $good_response_codes ) ) {
+                require_once dirname( __FILE__ ) .
+                    '/../library/StaticHtmlOutput/WsLog.php';
+                WsLog::l(
+                    'BAD RESPONSE STATUS (' . $status_code . '): '
+                );
+
+                throw new Exception( 'Netlify API bad response status' );
+            }
+
             $response_elements =
-                json_decode( (string) $response->getBody(), true );
+                json_decode( (string) $output, true );
 
             if ( isset( $response_elements['updated_at'] ) ) {
                 echo 'Last updated at: ' . $response_elements['updated_at'];
@@ -135,7 +175,6 @@ class StaticHtmlOutput_Netlify {
                 '/../library/StaticHtmlOutput/WsLog.php';
             WsLog::l( 'NETLIFY TEST ERROR' );
             WsLog::l( $e );
-            error_log( $e );
             throw new Exception( $e );
         }
     }
