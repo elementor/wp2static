2bc8a18d6ac64460bde39c6263d9dfd7c31ea34d
Leon Stafford <lionhive@gmail.com>
2019-01-13 16:10:46 +0700

    expand linting to tests, deploy_modules;rm cruft files
    Former-commit-id: 8ab055817054655dbf9a1ebbeab08f7481db5b88

--- provisioning/run_code_quality_check.sh
+++ provisioning/run_code_quality_check.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$HOME/PHP_CodeSniffer/bin/phpcs --ignore=*/powerpack/*,*/provisioning/*,*/GuzzleHttp/*,*/Github/*,*/aws/*,*/CloudFront/*,*/Psr/*,*/CSSParser/*,*/FTP/*,*/S3/*,*/URL2/*,wp2static.php ./
+$HOME/PHP_CodeSniffer/bin/phpcs --ignore=*/powerpack/*,*/provisioning/*.sh,*/provisioning/documentation*,*/Github/*,*/CloudFront/*,*/CSSParser/*,*/FTP/*,*/URL2/*,wp2static.php ./
