b65895658194be07cea8e2ce834ab166ef0070ce
Leon Stafford <lionhive@gmail.com>
2019-01-15 19:04:33 +0700

    GitHub deploy support diff-only
    Former-commit-id: e2ad65441a39c73704078c4ed1a236d23ae2bbfd

--- provisioning/deployment_modules/GitHub.php
+++ provisioning/deployment_modules/GitHub.php
@@ -12,13 +12,17 @@ class StaticHtmlOutput_GitHub extends StaticHtmlOutput_SitePublisher {
 
         $this->api_base = 'https://api.github.com/repos/';
 
+        $this->previous_hashes_path =
+            $this->settings['wp_uploads_path'] .
+                '/WP2STATIC-GITLAB-PREVIOUS-HASHES.txt';
+
         if ( defined( 'WP_CLI' ) ) { return; }
 
         switch ( $_POST['ajax_action'] ) {
             case 'github_prepare_export':
                 $this->bootstrap();
                 $this->loadArchive();
-                $this->prepare_export( true );
+                $this->prepareDeploy( true );
                 break;
             case 'github_upload_files':
                 $this->bootstrap();
@@ -32,7 +36,7 @@ class StaticHtmlOutput_GitHub extends StaticHtmlOutput_SitePublisher {
     }
 
     public function upload_files() {
-        $files_remaining = $this->get_remaining_items_count();
+        $this->files_remaining = $this->getRemainingItemsCount();
 
         if ( $this->files_remaining < 0 ) { echo 'ERROR'; die(); }
 
@@ -40,9 +44,11 @@ class StaticHtmlOutput_GitHub extends StaticHtmlOutput_SitePublisher {
 
         $batch_size = $this->settings['deployBatchSize'];
 
-        if ( $batch_size > $files_remaining ) {
-            $batch_size = $files_remaining;
+        if ( $batch_size > $this->files_remaining ) {
+            $batch_size = $this->files_remaining;
         }
+
+        $lines = $this->getItemsToDeploy( $batch_size );
 
         $this->openPreviousHashesFile();
 
@@ -105,7 +111,7 @@ JSON;
                 array( '200', '201', '301', '302', '304' )
             );
 
-            $gh_file_info = json_decode( $this->body, true );
+            $gh_file_info = json_decode( $client->body, true );
 
             $existing_file_object =
                 $gh_file_info['data']['repository']['object'];
@@ -191,95 +197,52 @@ JSON;
                 }
 
                 try {
-                    $ch = curl_init();
-
-                    curl_setopt( $ch, CURLOPT_URL, $remote_path );
-                    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-                    curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0 );
-                    curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0 );
-                    curl_setopt( $ch, CURLOPT_HEADER, 0 );
-                    curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1 );
-                    curl_setopt( $ch, CURLOPT_POST, 1 );
-                    curl_setopt( $ch, CURLOPT_USERAGENT, 'WP2Static.com' );
-                    curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, 'PUT' );
-
                     $post_options = array(
                         'message' => $commit_message,
                         'content' => $b64_file_contents,
                         'branch' => $this->settings['ghBranch'],
                     );
 
-                    curl_setopt(
-                        $ch,
-                        CURLOPT_POSTFIELDS,
-                        json_encode( $post_options )
+                    $headers = array(
+                        'Authorization: ' .
+                                'token ' . $this->settings['ghToken']
                     );
 
-                    curl_setopt(
-                        $ch,
-                        CURLOPT_HTTPHEADER,
-                        array(
-                            'Authorization: ' .
-                                'token ' . $this->settings['ghToken'],
+                    $client->putWithJSONPayloadCustomHeaders(
+                        $remote_path,
+                        $post_options,
+                        $headers,
+                        $curl_options = array(
+                            CURLOPT_SSLVERSION => CURL_SSLVERSION_TLSv1_2,
                         )
                     );
 
-                    $output = curl_exec( $ch );
-                    $status_code = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
+                    $this->checkForValidResponses(
+                        $client->status_code,
+                        array( '200', '201', '301', '302', '304' )
+                    );
 
-                    curl_close( $ch );
-
-                    $good_response_codes =
-                        array( '200', '201', '301', '302', '304' );
-
-                    if ( ! in_array( $status_code, $good_response_codes ) ) {
-                        require_once dirname( __FILE__ ) .
-                            '/../library/StaticHtmlOutput/WsLog.php';
-                        WsLog::l(
-                            'BAD RESPONSE STATUS (' . $status_code . '): '
-                        );
-
-                        throw new Exception( 'GitHub API bad response status' );
-                    }
+                    $this->writeFilePathAndHashesToFile();
                 } catch ( Exception $e ) {
-                    require_once dirname( __FILE__ ) .
-                        '/../library/StaticHtmlOutput/WsLog.php';
-                    WsLog::l( 'GITHUB EXPORT: error encountered' );
-                    WsLog::l( $e );
-                    throw new Exception( $e );
-                    return;
+                    $this->handleException( $e );
                 }
             }
 
-            $batch_index++;
+            $this->recordFilePathAndHashInMemory(
+                $target_path,
+                $local_file_contents
+            );
 
-            $completed_urls =
-                $total_urls_to_crawl -
-                $files_remaining +
-                $batch_index;
-
-            require_once dirname( __FILE__ ) .
-                '/../library/StaticHtmlOutput/ProgressLog.php';
-            ProgressLog::l( $completed_urls, $total_urls_to_crawl );
+            // NOTE: delay and progress askew in GitLab as we may
+            // upload all in one  request. Progress indicates building
+            // of list of files that will be deployed/checking if different
+            $this->updateProgress();
         }
 
-        if ( isset( $this->settings['ghBlobDelay'] ) &&
-            $this->settings['ghBlobDelay'] > 0 ) {
-            sleep( $this->settings['ghBlobDelay'] );
-        }
+        $this->pauseBetweenAPICalls();
 
-        $files_remaining = $this->get_remaining_items_count();
-
-        if ( $files_remaining > 0 ) {
-            if ( defined( 'WP_CLI' ) ) {
-                $this->upload_files();
-            } else {
-                echo $files_remaining;
-            }
-        } else {
-            if ( ! defined( 'WP_CLI' ) ) {
-                echo 'SUCCESS';
-            }
+        if ( $this->uploadsCompleted() ) {
+            $this->finalizeDeployment();
         }
     }
 
