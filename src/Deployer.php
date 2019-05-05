2fccb7aee448366d55c2f5557f42a969cd4666eb
Leon <leon@wp2static.com>
2019-05-05 21:48:44 +0700

    covering unexpected conditions

--- src/Deployer.php
+++ src/Deployer.php
@@ -2,6 +2,8 @@
 
 namespace WP2Static;
 
+use WP_CLI;
+
 class Deployer extends Base {
 
     public function __construct() {
@@ -24,137 +26,7 @@ class Deployer extends Base {
 
         $start_time = microtime( true );
 
-        $deployers_dir = dirname( __FILE__ ) . '/../deployers';
-
-        switch ( $this->settings['selected_deployment_option'] ) {
-            case 'folder':
-                break;
-            case 'zip':
-                break;
-            case 's3':
-                require_once dirname( __FILE__ ) .
-                    '/../WP2Static/SitePublisher.php';
-
-                require_once $deployers_dir . '/S3.php';
-
-                if ( $test ) {
-                    error_log( 'testing s3 deploy' );
-                    $s3->test_s3();
-                    return;
-                }
-
-                $s3->bootstrap();
-                $s3->loadArchive();
-                $s3->prepareDeploy();
-                $s3->upload_files();
-                $s3->cloudfront_invalidate_all_items();
-                break;
-            case 'bitbucket':
-                require_once dirname( __FILE__ ) .
-                    '/../WP2Static/SitePublisher.php';
-
-                require_once $deployers_dir . '/Bitbucket.php';
-
-                if ( $test ) {
-                    error_log( 'testing bitbucket deploy' );
-                    $bitbucket->test_upload();
-                    return;
-                }
-
-                $bitbucket->bootstrap();
-                $bitbucket->loadArchive();
-                $bitbucket->prepareDeploy( true );
-                $bitbucket->upload_files();
-                break;
-            case 'bunnycdn':
-                require_once dirname( __FILE__ ) .
-                    '/../WP2Static/SitePublisher.php';
-
-                require_once $deployers_dir . '/BunnyCDN.php';
-
-                if ( $test ) {
-                    error_log( 'testing BunnyCDN deploy' );
-                    $bunny->test_deploy();
-                    return;
-                }
-
-                $bunny->bootstrap();
-                $bunny->loadArchive();
-                $bunny->prepareDeploy( true );
-                $bunny->upload_files();
-                $bunny->purge_all_cache();
-                break;
-            case 'ftp':
-                require_once dirname( __FILE__ ) .
-                    '/../WP2Static/SitePublisher.php';
-
-                require_once $deployers_dir . '/FTP.php';
-
-                if ( $test ) {
-                    error_log( 'testing FTP deploy' );
-                    $ftp->test_ftp();
-                    return;
-                }
-
-                $ftp->bootstrap();
-                $ftp->loadArchive();
-                $ftp->prepareDeploy();
-                $ftp->upload_files();
-                break;
-            case 'github':
-                require_once dirname( __FILE__ ) .
-                    '/../WP2Static/SitePublisher.php';
-
-                require_once $deployers_dir . '/GitHub.php';
-
-                if ( $test ) {
-                    error_log( 'testing GitHub deploy' );
-                    $github->test_upload();
-                    return;
-                }
-
-                $github->bootstrap();
-                $github->loadArchive();
-                $github->prepareDeploy( true );
-                $github->upload_files();
-                break;
-            case 'gitlab':
-                require_once dirname( __FILE__ ) .
-                    '/../WP2Static/SitePublisher.php';
-
-                require_once $deployers_dir . '/GitLab.php';
-
-                if ( $test ) {
-                    error_log( 'testing GitLab deploy' );
-                    $gitlab->test_file_create();
-                    return;
-                }
-
-                $gitlab->bootstrap();
-                $gitlab->loadArchive();
-                $gitlab->getListOfFilesInRepo();
-
-                $gitlab->prepareDeploy( true );
-                $gitlab->upload_files();
-                break;
-            case 'netlify':
-                require_once dirname( __FILE__ ) .
-                    '/../WP2Static/SitePublisher.php';
-
-                require_once $deployers_dir . '/Netlify.php';
-
-                if ( $test ) {
-                    error_log( 'testing Netlify deploy' );
-                    $gitlab->loadArchive();
-                    $netlify->test_netlify();
-                    return;
-                }
-
-                $netlify->bootstrap();
-                $netlify->loadArchive();
-                $netlify->deploy();
-                break;
-        }
+        // TODO: reimplement CLI based deployment for add-ons / local
 
         $end_time = microtime( true );
 
@@ -162,7 +34,7 @@ class Deployer extends Base {
 
         WP_CLI::success(
             'Deployed to: ' . $method . ' in ' .
-            date( 'H:i:s', $duration )
+            date( 'H:i:s', (int) $duration )
         );
 
         $this->finalizeDeployment();
