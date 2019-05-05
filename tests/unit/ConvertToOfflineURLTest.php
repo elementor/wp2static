3b2eed3d78e53408e3a5744c340d96dee3181f09
Leon <leon@wp2static.com>
2019-05-05 23:20:16 +0700

    dataProvider;failing test for ConvertToOfflineURL

--- tests/unit/ConvertToOfflineURLTest.php
+++ tests/unit/ConvertToOfflineURLTest.php
@@ -6,18 +6,36 @@ use PHPUnit\Framework\TestCase;
 
 final class ConvertToOfflineURLTest extends TestCase{
 
-    public function testaddsRelativePathToURL() {
-        $url_to_change = 'mytheme/assets/link-to-an-image.jpg';
-        $page_url = 'https://myplaceholderdomain.com/some-post/';
-        $placeholder_url = 'https://myplaceholderdomain.com/';
-
+    /**
+     * @dataProvider offlineURLConversionProvider
+     */
+    public function testaddsRelativePathToURL(
+        $url_to_change, $page_url, $placeholder_url, $expectation
+    ) {
         $converted_url = ConvertToOfflineURL::convert(
             $url_to_change, $page_url, $placeholder_url
         );
 
         $this->assertEquals(
-            '../mytheme/assets/link-to-an-image.jpg',
+            $expectation,
             $converted_url
         );
+    }
+
+    public function offlineURLConversionProvider() {
+        return [
+           'document relative asset' =>  [
+                'mytheme/assets/link-to-an-image.jpg',
+                'https://myplaceholderdomain.com/some-post/',
+                'https://myplaceholderdomain.com/',
+                '../mytheme/assets/link-to-an-image.jpg'
+            ],
+           'root relative asset' =>  [
+                '/mytheme/assets/link-to-an-image.jpg',
+                'https://myplaceholderdomain.com/some-post/',
+                'https://myplaceholderdomain.com/',
+                '../../mytheme/assets/link-to-an-image.jpg'
+            ],
+        ];
     }
 }
