93ba832098073448aba972870d8e188ab4ff47c6
Leon <leon@wp2static.com>
2019-05-16 14:31:25 +0700

    build cmd without dev deps

--- tools/build_release.sh
+++ tools/build_release.sh
@@ -13,6 +13,12 @@ rm -Rf $TMP_DIR/static-html-output-plugin
 mkdir $TMP_DIR/static-html-output-plugin
 
 
+# clear dev dependencies
+rm -Rf $EXEC_DIR/vendor/*
+# load prod deps
+composer install --no-dev
+
+
 cp -r $EXEC_DIR/languages $TMP_DIR/static-html-output-plugin/
 cp -r $EXEC_DIR/src $TMP_DIR/static-html-output-plugin/
 cp -r $EXEC_DIR/vendor $TMP_DIR/static-html-output-plugin/
@@ -28,7 +34,6 @@ find . -type d -exec chmod 755 {} \;
 find . -type f -exec chmod 644 {} \;
 
 # strip comments and whitespace from each PHP file
-
 if [ -z "$2" ]; then
   find .  ! -name 'wp2static.php' -name \*.php -exec $EXEC_DIR/tools/compress_php_file {} \;
 fi
