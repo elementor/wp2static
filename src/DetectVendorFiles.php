728e67384fcce7ba0f75b00bb754d750e8b887fa
Leon <leon@wp2static.com>
2019-05-17 07:11:39 +0700

    rm filepath detections for Elementor;Soliloquoy in favor of flexible dir

--- src/DetectVendorFiles.php
+++ src/DetectVendorFiles.php
@@ -9,23 +9,7 @@ class DetectVendorFiles {
     public static function detect( $wp_site_url ) {
         $vendor_files = array();
 
-        /*
-            This is less needed if bulk detecting via DetectPluginAssets
-            should be moved into it's own Elementor Add-on, to seamlessly
-            handle forms, search and anything else to make seamless Elementor
-            to static workflow
-        */
-        if ( class_exists( '\\Elementor\Api' ) ) {
-            $elementor_font_dir = SiteInfo::getPath( 'plugins' ) .
-                'elementor/assets/lib/font-awesome';
-
-            $elementor_urls = FilesHelper::getListOfLocalFilesByUrl(
-                $elementor_font_dir
-            );
-
-            $vendor_files = array_merge( $vendor_files, $elementor_urls );
-        }
-
+        // Yoast
         if ( defined( 'WPSEO_VERSION' ) ) {
             $yoast_sitemaps = array(
                 '/sitemap_index.xml',
@@ -38,23 +22,11 @@ class DetectVendorFiles {
             $vendor_files = array_merge( $vendor_files, $yoast_sitemaps );
         }
 
-        if ( is_dir( SiteInfo::getPath( 'plugins' ) . 'soliloquy/' ) ) {
-            $soliloquy_assets = SiteInfo::getPath( 'plugins' ) .
-                'soliloquy/assets/css/images/';
-
-            $soliloquy_urls = FilesHelper::getListOfLocalFilesByUrl(
-                $soliloquy_assets
-            );
-
-            $vendor_files = array_merge( $vendor_files, $soliloquy_urls );
-        }
-
         // cache dir used by Autoptimize and other themes/plugins
         $vendor_cache_dir =
             SiteInfo::getPath( 'content' ) . 'cache/';
 
         if ( is_dir( $vendor_cache_dir ) ) {
-
             $site_url = SiteInfo::getUrl( 'site' );
             $content_url = SiteInfo::getUrl( 'content' );
 
@@ -112,17 +84,6 @@ class DetectVendorFiles {
                     $custom_permalinks
                 );
             }
-        }
-
-        if ( class_exists( 'molongui_authorship' ) ) {
-            $molongui_path = SiteInfo::getPath( 'plugins' ) .
-                'molongui-authorship';
-
-            $molongui_urls = FilesHelper::getListOfLocalFilesByUrl(
-                $molongui_path
-            );
-
-            $vendor_files = array_merge( $vendor_files, $molongui_urls );
         }
 
         return $vendor_files;
