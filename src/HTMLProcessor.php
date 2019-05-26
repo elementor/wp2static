b115ae40535b581230d338b256c519913d636381
Leon <leon@wp2static.com>
2019-05-27 00:47:17 +0700

    rm debug

--- src/HTMLProcessor.php
+++ src/HTMLProcessor.php
@@ -378,15 +378,11 @@ class HTMLProcessor extends Base {
         list( $url, $attribute_to_change ) =
             $this->getURLAndTargetAttribute( $element );
 
-        error_log($url);
-
         if ( ! $url || ! $attribute_to_change ) {
             return;
         }
 
         $url = $this->rewriteLocalURL( $url );
-
-        error_log($url);
 
         return $element->setAttribute( $attribute_to_change, $url );
     }
