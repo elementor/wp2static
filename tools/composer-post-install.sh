f8dd08de11358d0ebe4dfc804c99daaf752ff18a
leon <leon@x230.example.com>
2020-01-11 06:32:47 +1030

    rm Mac-centric script condition

--- tools/composer-post-install.sh
+++ tools/composer-post-install.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-if hash gsed 2>/dev/null; then
-  gsed -e 's#^function is_countable(#// &#' -i vendor/giacocorsiglia/wordpress-stubs/wordpress-stubs.php
-else
-  sed -e 's#^function is_countable(#// &#' -i vendor/giacocorsiglia/wordpress-stubs/wordpress-stubs.php
-fi
+sed -e 's#^function is_countable(#// &#' -i vendor/giacocorsiglia/wordpress-stubs/wordpress-stubs.php
 
