3403c75d51437532664cd94fa932177df4bc89a2
Leon <me@ljs.dev>
2020-02-24 01:45:55 +1030

    rm unused

--- tests/code_quality.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-$HOME/PHP_CodeSniffer/bin/phpcs --standard=./tools/phpcs.xml --ignore=,*/tests/*,*/CSSParser/*,*/FTP/*,*/URL2/*,wp2static.php ./
-
-$HOME/PHP_CodeSniffer/bin/phpcs -p . --standard=PHPCompatibility --runtime-set testVersion 5.6
