e0c088db199a5f990a509d38db2c336d25cecf74
Leon <me@ljs.dev>
2020-03-08 03:15:52 +1030

    drop wp2static tables on uninstall

--- tools/build_release.sh
+++ tools/build_release.sh
@@ -33,6 +33,7 @@ cp -r $EXEC_DIR/assets $TMP_DIR/static-html-output-plugin/
 cp -r $EXEC_DIR/vendor $TMP_DIR/static-html-output-plugin/
 cp -r $EXEC_DIR/readme.txt $TMP_DIR/static-html-output-plugin/
 cp -r $EXEC_DIR/views $TMP_DIR/static-html-output-plugin/
+cp -r $EXEC_DIR/uninstall.php $TMP_DIR/static-html-output-plugin/
 cp -r $EXEC_DIR/wp2static.php $TMP_DIR/static-html-output-plugin/
 cp -r $EXEC_DIR/admin $TMP_DIR/static-html-output-plugin/
 cp -r $EXEC_DIR/js $TMP_DIR/static-html-output-plugin/
---
+++ uninstall.php
@@ -0,0 +1,23 @@
+<?php
+
+// exit uninstall if not called by WP
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit();
+}
+
+global $wpdb;
+
+$tables_to_drop = [
+    'wp2static_core_options',
+    'wp2static_crawl_cache',
+    'wp2static_deploy_cache',
+    'wp2static_jobs',
+    'wp2static_log',
+    'wp2static_urls',
+];
+
+foreach ( $tables_to_drop as $table ) {
+    $table_name = $wpdb->prefix . $table;
+
+    $wpdb->query( "DROP TABLE IF EXISTS $table_name" );
+}
