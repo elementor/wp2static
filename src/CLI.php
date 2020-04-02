a5b34ce053d65d92bdc13d52f66345d58af07150
John Shaffer <jshaffer2112@protonmail.com>
2020-04-02 14:20:56 +1030

    WP-CLI cmd process_queue
     - change help text on Jobs page
     - getWaitingJobs() to JobsQueue
     - process_queue() to CLI

--- src/CLI.php
+++ src/CLI.php
@@ -343,6 +343,23 @@ class CLI {
     }
 
     /**
+     * Process any jobs in the queue.
+     */
+    public function process_queue() : void {
+        $job_count = JobQueue::getWaitingJobs();
+
+        if ( $job_count === 0 ) {
+            WP_CLI::success( 'No jobs in queue' );
+        } else {
+            WP_CLI::line( ' Processing ' . $job_count . ' job' . ( $job_count > 1 ? 's' : '' ) );
+
+            Controller::wp2static_process_queue();
+
+            WP_CLI::success( 'Done processing queue' );
+        }
+    }
+
+    /**
      * Crawl Queue
      *
      * <list>
