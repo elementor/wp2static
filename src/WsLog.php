3c85bc4b82a147cf47bfed9462136efad8a6f859
flynsarmy <flynsarmy@gmail.com>
2020-08-12 14:11:36 +1000

    Remove unnecessary processing

--- src/WsLog.php
+++ src/WsLog.php
@@ -67,11 +67,7 @@ class WsLog {
 
         $table_name = $wpdb->prefix . 'wp2static_log';
 
-        $rows = $wpdb->get_results( "SELECT time, log FROM $table_name ORDER BY id DESC" );
-
-        foreach ( $rows as $row ) {
-            $logs[] = $row;
-        }
+        $logs = $wpdb->get_results( "SELECT time, log FROM $table_name ORDER BY id DESC" );
 
         return $logs;
     }
