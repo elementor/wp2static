40cc8f579fa534d33c71d7aa7c8b9bc2d1146009
Leon <me@ljs.dev>
2020-12-10 14:28:33 +1100

    update dep refs in src;test composer

--- src/Crawler.php
+++ src/Crawler.php
@@ -9,8 +9,8 @@
 namespace WP2Static;
 
 use WP2StaticGuzzleHttp\Client;
-use GuzzleHttp\Psr7\Request;
-use GuzzleHttp\Psr7\Response;
+use WP2StaticGuzzleHttp\Psr7\Request;
+use WP2StaticGuzzleHttp\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
 
 define( 'WP2STATIC_REDIRECT_CODES', [ 301, 302, 303, 307, 308 ] );
