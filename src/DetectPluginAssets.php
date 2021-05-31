a312aa70fea26555320d1732a9127a9c80b3a456
Stefan Ullinger <stefan@slicemenice.de>
2021-05-31 08:07:52 +0200

    merged code to avoid unnecessary "temporary variable assignments"

--- src/DetectPluginAssets.php
+++ src/DetectPluginAssets.php
@@ -29,11 +29,11 @@ class DetectPluginAssets {
             $active_plugins = get_option( 'active_plugins' );
 
             if ( is_multisite() ) {
-                $active_sitewide_plugins = get_site_option( 'active_sitewide_plugins' );
-                $active_sitewide_plugins = array_keys( $active_sitewide_plugins );
-
                 $active_plugins = array_unique(
-                    array_merge( $active_plugins, $active_sitewide_plugins )
+                    array_merge(
+                        $active_plugins,
+                        array_keys( get_site_option( 'active_sitewide_plugins' ) )
+                    )
                 );
             }
 
