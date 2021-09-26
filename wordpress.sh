05c4aa27c5ed7092795d1dbf5080942e0144db24
John Shaffer <jshaffer2112@protonmail.com>
2021-09-25 23:30:01 -0500

    Configure and install WordPress.

--- wordpress.sh
+++ wordpress.sh
@@ -4,3 +4,9 @@ rm -rf wordpress
 WP_DIR=`echo $buildInputs | tr " " "\n" | grep wordpress`
 cp -r $WP_DIR/share/wordpress .
 chmod +w -R wordpress
+
+mysql --socket=mariadb/data/mysql.sock -e "CREATE USER IF NOT EXISTS 'wordpress'@'localhost' IDENTIFIED BY '8BVMm2jqDE6iADNyfaVCxoCzr3eBY6Ep'; CREATE DATABASE IF NOT EXISTS wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost'; FLUSH PRIVILEGES;"
+
+cp wp-config.php wordpress
+wp core install --path=wordpress --url="https://example.com" --title=WordPress --admin_user=user --admin_email="user@example.com"
+wp option update permalink_structure "/%postname%/" --path=wordpress
