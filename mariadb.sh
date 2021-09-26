b81334fec379b65fdb07e655f5e86265c96717c1
John Shaffer <jshaffer2112@protonmail.com>
2021-09-25 23:29:43 -0500

    Delete mariadb data on start.

--- mariadb.sh
+++ mariadb.sh
@@ -1,1 +1,6 @@
-mysql_install_db --defaults-file="mariadb/my.cnf" --datadir="$PWD/mariadb/data" && mysqld --defaults-file="mariadb/my.cnf" -h "$PWD/mariadb/data"
+set -e
+
+rm -rf mariadb/data/*
+touch mariadb/data/.keep
+mysql_install_db --defaults-file="mariadb/my.cnf" --datadir="$PWD/mariadb/data"
+mysqld --defaults-file="mariadb/my.cnf" -h "$PWD/mariadb/data"
