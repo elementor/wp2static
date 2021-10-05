4e917d4c4954cad49726df425d31f409014addc0
John Shaffer <jshaffer2112@protonmail.com>
2021-10-05 01:22:20 -0500

    Have WP connect to MySQL over a Unix socket
    This is faster than connecting over a TCP/IP socket.

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'wordpress' );
 define( 'DB_PASSWORD', '8BVMm2jqDE6iADNyfaVCxoCzr3eBY6Ep' );
 
 /** MySQL hostname */
-define( 'DB_HOST', '127.0.0.1:7001' );
+define( 'DB_HOST', ':' . dirname( __FILE__) . '/../mariadb/data/mysql.sock' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
