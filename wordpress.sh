a88f0456f49f83ea241c623ccccc6e8edf19a570
John Shaffer <jshaffer2112@protonmail.com>
2021-10-05 01:09:03 -0500

    Set admin password to "pass" on WP init.

--- wordpress.sh
+++ wordpress.sh
@@ -11,5 +11,5 @@ done
 mysql --socket=mariadb/data/mysql.sock -e "CREATE USER IF NOT EXISTS 'wordpress'@'localhost' IDENTIFIED BY '8BVMm2jqDE6iADNyfaVCxoCzr3eBY6Ep'; CREATE DATABASE IF NOT EXISTS wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost'; FLUSH PRIVILEGES;"
 
 cp wp-config.php wordpress
-wp core install --path=wordpress --url="https://example.com" --title=WordPress --admin_user=user --admin_email="user@example.com"
+wp core install --path=wordpress --url="https://example.com" --title=WordPress --admin_user=user --admin_email="user@example.com" --admin_password=pass
 wp option update permalink_structure "/%postname%/" --path=wordpress
