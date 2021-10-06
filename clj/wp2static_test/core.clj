c7c948a4268cdf973b6cac1680d55f907113cf9e
John Shaffer <jshaffer2112@protonmail.com>
2021-10-05 21:31:00 -0500

    Separate core and main. Main is for the system-map

---
+++ clj/wp2static_test/core.clj
@@ -0,0 +1,47 @@
+(ns wp2static-test.core
+  (:require [clojure.java.shell :as sh]
+            [clojure.tools.logging.readable :as log])
+  (:use popen))
+
+(defn sh! [& args]
+  (let [{:keys [exit] :as result} (apply sh/sh args)]
+    (if (zero? exit)
+      result
+      (throw (ex-info (str "Subprocess returned with exit code " exit)
+               {:args args
+                :result result})))))
+
+(defn build-wp2static! []
+  (let [fname (str "wp2static-" (System/currentTimeMillis))
+        zip-name (str fname ".zip")
+        plugins-dir "wordpress/wp-content/plugins"]
+    (try
+      (sh! "bash" "./tools/build_release.sh" fname
+        :dir (str (System/getenv "PWD") "/../wp2static"))
+      (sh! "mv" (str (System/getenv "HOME") "/Downloads/" zip-name) "."
+        :dir plugins-dir)
+      (sh! "rm" "-rf" "wp2static" :dir plugins-dir)
+      (sh! "unzip" zip-name :dir plugins-dir)
+      (sh! "rm" zip-name :dir plugins-dir)
+      (sh! "wp" "plugin" "activate" "wp2static" "--path=wordpress")
+      (finally
+        (sh/sh "rm" zip-name :dir plugins-dir)))))
+
+(defn log-exit-code! [name process]
+  (log/info name "exited with code" (exit-code process)))
+
+(defn log-stderr! [name process]
+  (with-open [reader (stderr process)]
+    (doseq [line (line-seq reader)]
+      (log/error name line))))
+
+(defn log-stdout! [process]
+  (with-open [reader (stdout process)]
+    (doseq [line (line-seq reader)]
+      (log/info name line))))
+
+(defn log-process! [name process]
+  (future (log-stderr! (str "[" name "]") process))
+  (future (log-stdout! (str "[" name "]") process))
+  (future (log-exit-code! name process))
+  process)
--- clj/wp2static_test/main.clj
+++ clj/wp2static_test/main.clj
@@ -1,52 +1,8 @@
 (ns wp2static-test.main
   (:require hashp.core
-            [clojure.java.shell :as sh]
-            [clojure.tools.logging.readable :as log]
-            [com.stuartsierra.component :as component])
+            [com.stuartsierra.component :as component]
+            [wp2static-test.core :as core])
   (:use popen))
-
-(defn sh! [& args]
-  (let [{:keys [exit] :as result} (apply sh/sh args)]
-    (if (zero? exit)
-      result
-      (throw (ex-info (str "Subprocess returned with exit code " exit)
-               {:args args
-                :result result})))))
-
-(defn build-wp2static! []
-  (let [fname (str "wp2static-" (System/currentTimeMillis))
-        zip-name (str fname ".zip")
-        plugins-dir "wordpress/wp-content/plugins"]
-    (try
-      (sh! "bash" "./tools/build_release.sh" fname
-        :dir (str (System/getenv "PWD") "/../wp2static"))
-      (sh! "mv" (str (System/getenv "HOME") "/Downloads/" zip-name) "."
-        :dir plugins-dir)
-      (sh! "rm" "-rf" "wp2static" :dir plugins-dir)
-      (sh! "unzip" zip-name :dir plugins-dir)
-      (sh! "rm" zip-name :dir plugins-dir)
-      (sh! "wp" "plugin" "activate" "wp2static" "--path=wordpress")
-      (finally
-        (sh/sh "rm" zip-name :dir plugins-dir)))))
-
-(defn log-exit-code! [name process]
-  (log/info name "exited with code" (exit-code process)))
-
-(defn log-stderr! [name process]
-  (with-open [reader (stderr process)]
-    (doseq [line (line-seq reader)]
-      (log/error name line))))
-
-(defn log-stdout! [process]
-  (with-open [reader (stdout process)]
-    (doseq [line (line-seq reader)]
-      (log/info name line))))
-
-(defn log-process! [name process]
-  (future (log-stderr! (str "[" name "]") process))
-  (future (log-stdout! (str "[" name "]") process))
-  (future (log-exit-code! name process))
-  process)
 
 (defn start-mariadb! []
   (popen ["bash" "mariadb.sh"]))
@@ -63,7 +19,7 @@
     (if process
       this
       (assoc this :process
-        (log-process! name (open-f this)))))
+        (core/log-process! name (open-f this)))))
   (stop [this]
     (if-not process
       this
@@ -78,7 +34,7 @@
   (shell-process
     {:name "MariaDB"
      :open-f (fn [_] (popen ["bash" "mariadb.sh"]))
-     :stop-f (fn [_] (sh! "mysqladmin"
+     :stop-f (fn [_] (core/sh! "mysqladmin"
                        "--socket=mariadb/data/mysql.sock"
                        "shutdown"))}))
 
@@ -89,7 +45,7 @@
                              "-p" (System/getenv "PWD")
                              "-c" "nginx.conf"
                              "-e" "stderr"]))
-     :stop-f (fn [_] (sh! "nginx"
+     :stop-f (fn [_] (core/sh! "nginx"
                        "-p" (System/getenv "PWD")
                        "-c" "nginx.conf"
                        "-e" "stderr"
