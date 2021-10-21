a1aa64f65ec64bb10dedcfa1e5371e7dec30e64f
John Shaffer <jshaffer2112@protonmail.com>
2021-10-20 23:47:21 -0500

    Ignore exit code on wp plugin uninstall
    It always returns 127 for some reason, which breaks our script
    due to the set -e option.

--- wordpress.sh
+++ wordpress.sh
@@ -13,4 +13,4 @@ mysql --socket=mariadb/data/mysql.sock -e "CREATE USER IF NOT EXISTS 'wordpress'
 cp wp-config.php wordpress
 wp core install --path=wordpress --url="https://example.com" --title=WordPress --admin_user=user --admin_email="user@example.com" --admin_password=pass
 wp option update permalink_structure "/%postname%/" --path=wordpress
-wp plugin uninstall --path=wordpress --deactivate --all
+wp plugin uninstall --path=wordpress --deactivate --all || true
