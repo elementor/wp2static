e3f209def46d194fba2afa1e94abc32277fdfaeb
John Shaffer <jshaffer2112@protonmail.com>
2021-10-26 18:11:53 -0500

    Work around `wp plugin uninstall` not doing its job

--- integration-tests/wordpress.sh
+++ integration-tests/wordpress.sh
@@ -15,4 +15,8 @@ mysql --socket=mariadb/data/mysql.sock -e "CREATE USER IF NOT EXISTS 'wordpress'
 cp wp-config.php wordpress
 wp core install --path=wordpress --url="https://example.com" --title=WordPress --admin_user=user --admin_email="user@example.com" --admin_password=pass
 wp option update permalink_structure "/%postname%/" --path=wordpress
-wp plugin uninstall --path=wordpress --deactivate --all || true
+# `wp plugin uninstall` doesn't successfuly remove the plugins,
+# so we deactivate them and delete them ourselves.
+wp plugin deactivate --path=wordpress --all
+rm -r wordpress/wp-content/plugins/*/
+rm -r wordpress/wp-content/plugins/hello.php
