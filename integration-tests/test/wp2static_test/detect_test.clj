f3a4fbab3561c505764ab44585762d61c3b9b5ac
John Shaffer <jshaffer2112@protonmail.com>
2021-11-04 22:30:09 -0500

    Fix exception when a sitemap URL returns 404

--- integration-tests/test/wp2static_test/detect_test.clj
+++ integration-tests/test/wp2static_test/detect_test.clj
@@ -6,7 +6,33 @@
 (defn get-crawled-file [path]
   (slurp (str "wordpress/wp-content/uploads/wp2static-crawled-site/" path)))
 
-(def robots-sitemap-slashes "User-agent: *
+(def robots-sitemap-404
+  "User-agent: *
+Disallow: /wp-admin/
+Allow: /wp-admin/admin-ajax.php
+
+Sitemap: http://localhost:7000/wp-content/sitemap.xml
+Sitemap: http://localhost:7000/does-not-exist.xml")
+
+(def sitemap-with-404
+  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<?xml-stylesheet type=\"text/xsl\" href=\"http://localhost:7000/wp-sitemap-index.xsl\" ?>
+<sitemapindex xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\"><sitemap><loc>http://localhost:7000/does-not-exist.xml</loc></sitemap></sitemapindex>")
+
+(deftest test-robots-404
+  (testing "robots.txt sitemap URLs that return 404s are ignored"
+    (test/with-test-system [_]
+      (try
+        (spit "wordpress/robots.txt" robots-sitemap-404)
+        (spit "wordpress/wp-content/sitemap.xml" sitemap-with-404)
+        (is (zero? (:exit (test/wp-cli!
+                            {:expect-warnings {#".*Got 404 for sitemap.*" 1}}
+                            "wp2static" "detect"))))
+        (finally
+          (test/sh! {} "rm" "wordpress/robots.txt" "wordpress/wp-content/sitemap.xml"))))))
+
+(def robots-sitemap-slashes
+  "User-agent: *
 Disallow: /wp-admin/
 Allow: /wp-admin/admin-ajax.php
 
