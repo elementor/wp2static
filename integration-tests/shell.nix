29d3ff3fcb7dd08b034b396ce3d2d75eda1ed9d0
John Shaffer <jdsha@proton.me>
2022-08-16 19:10:16 -0500

    Use niv to pin nix commits

--- integration-tests/shell.nix
+++ integration-tests/shell.nix
@@ -1,11 +1,6 @@
+{ sources ? import ../nix/sources.nix, pkgs ? import sources.nixpkgs { } }:
 let
-  rev = "3eb07eeafb52bcbf02ce800f032f18d666a9498d";
-  nixpkgs = fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-    sha256 = "1ah1fvll0z3w5ykzc6pabqr7mpbnbl1i3vhmns6k67a4y7w0ihrr";
-  };
-  pkgs = import nixpkgs {};
-  default = import ./default.nix;
+  default = import ./default.nix {};
 in with pkgs;
 mkShell {
   buildInputs = default;
