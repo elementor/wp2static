c2a6f4bbd8db5d42e5e843f33f45457bd1b273c8
Leon Stafford <me@ljs.dev>
2023-01-12 02:14:49 +1100

    add CTA buttons to admin notice

--- src/AdminNotices.php
+++ src/AdminNotices.php
@@ -29,10 +29,17 @@ class AdminNotices {
         $secondary_button_url = 'https://www.strattic.com/static-tools/?utm_campaign=learn-more&utm_source=wp2static&utm_medium=wp-dash&utm_term=general&utm_content=wp-notification-banner';
 
         printf(
-            '<div class="%1$s"><b>%2$s</b><p>%3$s</p></div>',
+            '<div class="%1$s"><b>%2$s</b><p>%3$s</p>' .
+            '<a href="%4$s" target="_blank"><button class="button button-primary">%5$s</button></a>' .
+            '<a href="%6$s" target="_blank"><button class="button button-secondary">%7$s</button></a>' .
+            '</div>',
             esc_attr( $class ),
             esc_html( $title ),
-            esc_html( $message )
+            esc_html( $message ),
+            esc_html( $primary_button_url ),
+            esc_html( $primary_button_title ),
+            esc_html( $secondary_button_url ),
+            esc_html( $secondary_button_title )
         );
     }
 }
